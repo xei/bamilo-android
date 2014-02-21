e88641ae615767e7109e7432c2e53253cd550c43
Filipe Carvalho <filipe.carvalho@africainternetgroup.com>
2014-02-21 15:24:23 +0000

    changes in shellaplication.java

--- Shell/src/com/rocket/framework/testshell/application/ShellApplication.java
+++ Shell/src/com/rocket/framework/testshell/application/ShellApplication.java
@@ -1,6 +1,7 @@
 package com.rocket.framework.testshell.application;
 
 import pt.rocket.framework.rest.RestClientSingleton;
+import pt.rocket.framework.rest.RestContract;
 import android.app.Application;
 
 public class ShellApplication extends Application {
@@ -8,7 +9,8 @@ public class ShellApplication extends Application {
 	  public void onCreate()
 	  {
 	    super.onCreate();
-	     
+	    
+	    RestContract.RUNNING_TESTS = true;
 	    // Initialize the singletons so their instances
 	    // are bound to the application process.
 	    RestClientSingleton.init(getApplicationContext());
