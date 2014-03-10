721e8155ee4343067e9520f30e59f2bf5d84d706
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-03-10 12:46:00 +0000

    % Fixed a NPE on CheckoutStepMng

--- pt.rocket.view/src/pt/rocket/utils/CheckoutStepManager.java
+++ pt.rocket.view/src/pt/rocket/utils/CheckoutStepManager.java
@@ -30,23 +30,33 @@ public class CheckoutStepManager {
         // Get the next step from json 
         String nextStep = null;
         
-        if(jsonObject.has(RestConstants.JSON_DATA_TAG) && 
-           jsonObject.optJSONObject(RestConstants.JSON_DATA_TAG).has(RestConstants.JSON_NATIVE_CHECKOUT_TAG)){
-            // From native checkout tag
-            JSONObject dataJson = jsonObject.optJSONObject(RestConstants.JSON_DATA_TAG);
-            JSONObject checkoutJson = dataJson.optJSONObject(RestConstants.JSON_NATIVE_CHECKOUT_TAG);
-            nextStep = checkoutJson.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
-        } else if(jsonObject.has(RestConstants.JSON_NATIVE_CHECKOUT_TAG)){
-            // From native checkout tag
-            JSONObject checkoutJson = jsonObject.optJSONObject(RestConstants.JSON_NATIVE_CHECKOUT_TAG);
-            nextStep = checkoutJson.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
-        } else if(jsonObject.has(RestConstants.JSON_NEXT_STEP_TAG)){
-            // From next step tag
-            nextStep = jsonObject.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
+        try {
+            
+            // Validate data
+            if(jsonObject.has(RestConstants.JSON_NATIVE_CHECKOUT_TAG)){
+                // From native checkout tag
+                JSONObject checkoutJson = jsonObject.optJSONObject(RestConstants.JSON_NATIVE_CHECKOUT_TAG);
+                nextStep = checkoutJson.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
+            } else if(jsonObject.has(RestConstants.JSON_NEXT_STEP_TAG)){
+                // From next step tag
+                nextStep = jsonObject.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
+            } else if (jsonObject.has(RestConstants.JSON_DATA_TAG) &&
+                        jsonObject.optJSONObject(RestConstants.JSON_DATA_TAG) != null &&
+                        jsonObject.optJSONObject(RestConstants.JSON_DATA_TAG).has(RestConstants.JSON_NATIVE_CHECKOUT_TAG)) {
+                // From data and native checkout tag
+                JSONObject dataJson = jsonObject.optJSONObject(RestConstants.JSON_DATA_TAG);
+                JSONObject checkoutJson = dataJson.optJSONObject(RestConstants.JSON_NATIVE_CHECKOUT_TAG);
+                nextStep = checkoutJson.optString(RestConstants.JSON_NEXT_STEP_TAG, null);
+            }
+                         
+            // Validate value
+            if(nextStep != null) return getNextFragment(nextStep);
+            else return FragmentType.UNKNOWN;
+                    
+        } catch (NullPointerException e) {
+            Log.w(TAG, "WARNING: JSON OBJECT IS NULL");
+            return FragmentType.UNKNOWN;
         }
-        // Validate value
-        if(nextStep != null) return getNextFragment(nextStep);
-        else return FragmentType.UNKNOWN;
     }
     
     /**
