fbe36a72d338e57d45cd50545313292e1d4314a1
manuelsilvaaig <manuel.silva@africainternetgroup.com>
2014-03-27 19:28:48 +0000

    Adding volley to the project. Done NAFAMZ-5221

--- pt.rocket.framework/src/pt/rocket/framework/utils/ProductSort.java
+++ pt.rocket.framework/src/pt/rocket/framework/utils/ProductSort.java
@@ -8,7 +8,7 @@ package pt.rocket.framework.utils;
  * 
  */
 public enum ProductSort {
-    NONE(-1), POPULARITY(0), NAME(1), PRICE(2), BRAND(3);
+    NONE(-1), POPULARITY(0), NAME(1), PRICE(2), BRAND(3), NEWIN(4), BESTRATING(5);
     
     public final int id;
     
