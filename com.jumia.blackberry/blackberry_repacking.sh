f5c17c555af78fe357994895ef2db82d69841e17
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 17:30:31 +0000

    % Updated the certicate using the Eclipse Tool and the shell script.

--- com.jumia.blackberry/blackberry_repacking.sh
+++ com.jumia.blackberry/blackberry_repacking.sh
@@ -8,30 +8,42 @@
 #echo Create apk
 #ant debug
 
+DEV=false
+
+##### Environment
+if [ $DEV ]; 
+then
+	echo "> Dev environment"
+	APK_PATH="com.jumia.blackberry/bin/com.jumia.blackberry.apk"
+	BAR_NAME="com.jumia.blackberry.bar"
+else
+	echo "> Release environment"
+	APK_PATH="com.jumia.blackberry/bin/Jumia-release.apk"
+	BAR_NAME="Jumia-release.bar"
+fi
+
 STOREPASS="sfp.1n.blackberry"
-APK_PATH="com.jumia.blackberry/bin/Jumia-release.apk"
 CNF_PATH="com.jumia.blackberry/assets/blackberry/android.cfg"
 MNF_PATH="com.jumia.blackberry/assets/blackberry/MANIFEST.MF"
 BAR_FOLDER="com.jumia.blackberry/dist/blackberry"
 CERT_FOLDER="com.jumia.blackberry/assets/blackberry/certificate"
 MAC_CRT_PATH="/Users/rocket/Library/Research\ In\ Motion"
-BAR_NAME="Jumia-release.bar"
 
-# Reload PATH
+#### Reload PATH (MAC OS)
 echo "> Reload PATH"
 . ~/.bash_profile
 
-# Generate certificate
+#### Generate certificate (TO CREATE A KEY)
 # blackberry-keytool -genkeypair -storepass $STOREPASS -dname "cn=Jumia"
 
-# Copy Certicate
+#### Copy Certicate
 echo "> Copy certificate"
-cp $CERT_FOLDER/* '$MAC_CRT_PATH'
+#cp $CERT_FOLDER/* "$MAC_CRT_PATH"
 
-# Repacking apk to bar
+#### Repacking apk to bar
 echo "> Repacking apk to bar"
 blackberry-apkpackager $APK_PATH $CNF_PATH -m $MNF_PATH -t $BAR_FOLDER -r
 
-# Sign Blackberry App
+#### Sign Blackberry App
 echo "> Sign Blackberry App"
 blackberry-signer -storepass $STOREPASS $BAR_FOLDER/$BAR_NAME
