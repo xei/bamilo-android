3a6f0971dca7b633ee81487adfea3c0577e213cb
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 17:05:30 +0000

    Added certificate

--- com.jumia.blackberry/blackberry_repacking.sh
+++ com.jumia.blackberry/blackberry_repacking.sh
@@ -13,7 +13,7 @@ APK_PATH="com.jumia.blackberry/bin/Jumia-release.apk"
 CNF_PATH="com.jumia.blackberry/assets/blackberry/android.cfg"
 MNF_PATH="com.jumia.blackberry/assets/blackberry/MANIFEST.MF"
 BAR_FOLDER="com.jumia.blackberry/dist/blackberry"
-CERT_FILE="com.jumia.blackberry/assets/blackberry/author.p12"
+CERT_FOLDER="com.jumia.blackberry/assets/blackberry/certificate"
 MAC_CRT_PATH="/Users/rocket/Library/Research\ In\ Motion"
 BAR_NAME="Jumia-release.bar"
 
@@ -21,11 +21,12 @@ BAR_NAME="Jumia-release.bar"
 echo "> Reload PATH"
 . ~/.bash_profile
 
-# Copy Certicate
-cp $CERT_FILE $MAC_CRT_PATH
-
 # Generate certificate
-# blackberry-keytool -genkeypair -storepass sfp.1n.blackberry -dname "cn=Jumia"
+# blackberry-keytool -genkeypair -storepass $STOREPASS -dname "cn=Jumia"
+
+# Copy Certicate
+echo "> Copy certificate"
+cp $CERT_FOLDER/* '$MAC_CRT_PATH'
 
 # Repacking apk to bar
 echo "> Repacking apk to bar"
@@ -33,8 +34,4 @@ blackberry-apkpackager $APK_PATH $CNF_PATH -m $MNF_PATH -t $BAR_FOLDER -r
 
 # Sign Blackberry App
 echo "> Sign Blackberry App"
-#blackberry-signer -bbidtoken bb_id_token.csk -storepass <PASS> ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
 blackberry-signer -storepass $STOREPASS $BAR_FOLDER/$BAR_NAME
-
-# Sign App
-#./batchbar-signer MyApp.bar mykey.p12 <<STOREPASS>> <<CSKPASS>>
