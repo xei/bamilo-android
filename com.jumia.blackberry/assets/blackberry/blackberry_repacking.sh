13905146297f044e9dd036e774bc8d5711b68fc4
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 16:06:05 +0000

    Added certificate

--- com.jumia.blackberry/assets/blackberry/blackberry_repacking.sh
+++ com.jumia.blackberry/assets/blackberry/blackberry_repacking.sh
@@ -8,9 +8,6 @@
 #echo Create apk
 #ant debug
 
-# Reload PATH
-. ~/.bash_profile
-
 STOREPASS="sfp.1n.blackberry"
 APK_PATH="./com.jumia.blackberry/bin/Jumia-release.apk"
 CNF_PATH="./com.jumia.blackberry/assets/blackberry/android.cfg"
@@ -18,12 +15,19 @@ MNF_PATH="./com.jumia.blackberry/assets/blackberry/MANIFEST.MF"
 BAR_FOLDER="./com.jumia.blackberry/dist/blackberry"
 BAR_NAME="Jumia-release.bar"
 
+# Reload PATH
+echo "> Reload PATH"
+. ~/.bash_profile
+
+# Generate certificate
+# blackberry-keytool -genkeypair -storepass sfp.1n.blackberry -dname "cn=Jumia"
+
 # Repacking apk to bar
-echo Repacking apk to bar
+echo "> Repacking apk to bar"
 blackberry-apkpackager $APK_PATH $CNF_PATH -m $MNF_PATH -t $BAR_FOLDER -r
 
-# Sign bar
-echo Repacking apk to bar
+# Sign Blackberry App
+echo "> Sign Blackberry App"
 #blackberry-signer -bbidtoken bb_id_token.csk -storepass <PASS> ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
 blackberry-signer -storepass $STOREPASS $BAR_FOLDER/$BAR_NAME
 
