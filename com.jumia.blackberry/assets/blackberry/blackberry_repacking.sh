e8b8def3a7660230ffbbdd37fb4b5909718a3e64
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 13:25:40 +0000

    Added the blackberry project

---
+++ com.jumia.blackberry/assets/blackberry/blackberry_repacking.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Create BlackBerry Account
+# user: laurent.detapol@jumia.com
+# pass: RocketInternet2014
+
+# Create apk
+#echo Create apk
+#ant debug
+
+STOREPASS="sfp.1n.blackberry"
+
+APK_PATH="./com.jumia.blackberry/bin/com.jumia.blackberry.apk"
+CNF_PATH="./com.jumia.blackberry/assets/blackberry/android.cfg"
+MNF_PATH="./com.jumia.blackberry/assets/blackberry/MANIFEST.MF"
+BAR_FOLDER="./com.jumia.blackberry/dist/blackberry"
+
+# Repacking apk to bar
+echo Repacking apk to bar
+blackberry-apkpackager $APK_PATH $CNF_PATH -m $MNF_PATH -t $BAR_FOLDER -r
+# -r -> find .apk, .cfg, .mf, and .png files recursively in specified folders
+# -m -> use custom manifest file(s); e.g. Helloworld.apk to use Helloworld.MF
+
+# Sign bar
+echo Repacking apk to bar
+#blackberry-signer -bbidtoken bb_id_token.csk -storepass sfp.1n.blackberry ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
+#blackberry-signer -storepass sfp.1n.blackberry ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
+
+
+# Sign App
+#./batchbar-signer MyApp.bar mykey.p12 <<STOREPASS>> <<CSKPASS>>
