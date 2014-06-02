947507556b6ec267fd2535be1bebf561cb050468
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 14:03:54 +0000

    % Updated the shell script file

--- com.jumia.blackberry/assets/blackberry/blackberry_repacking.sh
+++ com.jumia.blackberry/assets/blackberry/blackberry_repacking.sh
@@ -14,6 +14,7 @@ APK_PATH="./com.jumia.blackberry/bin/com.jumia.blackberry.apk"
 CNF_PATH="./com.jumia.blackberry/assets/blackberry/android.cfg"
 MNF_PATH="./com.jumia.blackberry/assets/blackberry/MANIFEST.MF"
 BAR_FOLDER="./com.jumia.blackberry/dist/blackberry"
+BAR_NAME="com.jumia.blackberry.bar"
 
 # Repacking apk to bar
 echo Repacking apk to bar
@@ -24,8 +25,7 @@ blackberry-apkpackager $APK_PATH $CNF_PATH -m $MNF_PATH -t $BAR_FOLDER -r
 # Sign bar
 echo Repacking apk to bar
 #blackberry-signer -bbidtoken bb_id_token.csk -storepass sfp.1n.blackberry ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
-#blackberry-signer -storepass sfp.1n.blackberry ./com.jumia.blackberry/dist/blackberry/com.jumia.blackberry.bar
-
+blackberry-signer -storepass sfp.1n.blackberry $BAR_FOLDER/$BAR_NAME
 
 # Sign App
 #./batchbar-signer MyApp.bar mykey.p12 <<STOREPASS>> <<CSKPASS>>
