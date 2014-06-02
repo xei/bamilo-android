8adc55c7e7a3ab0fa36428aeeb95a60878cf1857
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-06-02 17:54:05 +0000

    % Updated script

--- com.jumia.blackberry/blackberry_repacking.sh
+++ com.jumia.blackberry/blackberry_repacking.sh
@@ -1,26 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Create BlackBerry Account
+##### Create BlackBerry Account
 # user: laurent.detapol@jumia.com
 # pass: RocketInternet2014
 
-# Create apk
+##### Create apk
 #echo Create apk
 #ant debug
 
-DEV=false
-
 ##### Environment
-if [ $DEV ]
-then
-	echo "> Dev environment"
-	APK_PATH="com.jumia.blackberry/bin/com.jumia.blackberry.apk"
-	BAR_NAME="com.jumia.blackberry.bar"
-else
-	echo "> Release environment"
-	APK_PATH="com.jumia.blackberry/bin/Jumia-release.apk"
-	BAR_NAME="Jumia-release.bar"
-fi
+DEV=false
 
 STOREPASS="sfp.1n.blackberry"
 CNF_PATH="com.jumia.blackberry/assets/blackberry/android.cfg"
@@ -29,16 +18,28 @@ BAR_FOLDER="com.jumia.blackberry/dist/blackberry"
 CERT_FOLDER="com.jumia.blackberry/assets/blackberry/certificate"
 MAC_CRT_PATH="/Users/rocket/Library/Research\ In\ Motion"
 
-#### Reload PATH (MAC OS)
-echo "> Reload PATH"
-. ~/.bash_profile
+
+##### Environment
+if $DEV
+then
+	echo "> Dev environment"
+	APK_PATH="com.jumia.blackberry/bin/com.jumia.blackberry.apk"
+	BAR_NAME="com.jumia.blackberry.bar"
+else
+	echo "> Release environment"
+	APK_PATH="com.jumia.blackberry/bin/Jumia-release.apk"
+	BAR_NAME="Jumia-release.bar"
+	#### Reload PATH (MAC OS)
+	echo "> Reload PATH"
+	. ~/.bash_profile
+fi
 
 #### Generate certificate (TO CREATE A KEY)
 # blackberry-keytool -genkeypair -storepass $STOREPASS -dname "cn=Jumia"
 
 #### Copy Certicate
 echo "> Copy certificate"
-#cp $CERT_FOLDER/* "$MAC_CRT_PATH"
+cp $CERT_FOLDER/* "$MAC_CRT_PATH"
 
 #### Repacking apk to bar
 echo "> Repacking apk to bar"
