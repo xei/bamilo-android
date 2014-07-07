3ba1408d4afc7bea12da6f0aa0f05b67bd1b1ddc
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-07-07 17:53:37 +0000

    + Added the AndroidSlidingTabStrip lib. % Updated the Campaign to support new tab strip, NAFAMZ-7521.

--- pt.rocket.view/src/pt/rocket/view/fragments/CampaignsFragment.java
+++ pt.rocket.view/src/pt/rocket/view/fragments/CampaignsFragment.java
@@ -3,10 +3,10 @@
  */
 package pt.rocket.view.fragments;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.EnumSet;
 
+import pt.rocket.framework.components.androidslidingtabstrip.SlidingTabLayout;
 import pt.rocket.framework.objects.TeaserCampaign;
 import pt.rocket.framework.utils.EventType;
 import pt.rocket.framework.utils.LogTagHelper;
@@ -14,20 +14,15 @@ import pt.rocket.utils.MyMenuItem;
 import pt.rocket.utils.NavigationAction;
 import pt.rocket.view.R;
 import android.app.Activity;
-import android.graphics.LinearGradient;
-import android.graphics.Paint;
-import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.TextView;
 import de.akquinet.android.androlog.Log;
 
 /**
@@ -50,7 +45,7 @@ public class CampaignsFragment extends BaseFragment {
 
     private ArrayList<TeaserCampaign> mCampaigns;
 
-    private PagerTabStrip mCampaignPagerTabStrip;
+    private SlidingTabLayout mCampaignPagerTabStrip;
     
     /**
      * Constructor via bundle
@@ -125,15 +120,15 @@ public class CampaignsFragment extends BaseFragment {
         // Get view pager 
         mCampaignPager = (ViewPager) view.findViewById(R.id.campaign_pager);
         // Get tab pager
-        mCampaignPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.campaign_pager_tab);
-        // Set tab pager
-        setTabStripLayoutSpec();
+        mCampaignPagerTabStrip = (SlidingTabLayout) view.findViewById(R.id.campaign_pager_tab);
+        mCampaignPagerTabStrip.setCustomTabView(R.layout.tab_simple_item, R.id.tab);
         // Validate the current view
         mCampaignPagerAdapter = (CampaignPagerAdapter) mCampaignPager.getAdapter();
         if(mCampaignPagerAdapter == null) {
             //Log.d(TAG, "CAMPAIGNS ADAPTER IS NULL");
             mCampaignPagerAdapter = new CampaignPagerAdapter(getChildFragmentManager(), mCampaigns);
             mCampaignPager.setAdapter(mCampaignPagerAdapter);
+            mCampaignPagerTabStrip.setViewPager(mCampaignPager);
         }
         // Show the pre selection
         mCampaignPager.setCurrentItem(selectedPosition, true);
@@ -256,75 +251,6 @@ public class CampaignsFragment extends BaseFragment {
             return mCampaigns.get(position).getTargetTitle().toUpperCase();
         }
         
-        //@Override
-        //public void restoreState(Parcelable state, ClassLoader loader) {
-        //    Log.d(TAG, "ON RESTORE STATE");
-        //    if(state == null) Log.d(TAG, "STATE IS NULL");
-        //    if(loader == null) Log.d(TAG, "LOADER IS NULL");
-        //    if(state != null && loader != null) super.restoreState(state, loader);
-        //}
     }
 
-    
-    
-    //private final int TAB_PREV_ID = 0;
-    private final int TAB_CURR_ID = 1;
-    //private final int TAB_NEXT_ID = 2;
-
-    private final int TAB_INDICATOR_HEIGHT = 0;
-    private final int TAB_UNDERLINE_HEIGHT = 1;
-    private final int TAB_STRIP_COLOR = android.R.color.transparent;
-    
-    /**
-     * Set some layout parameters that aren't possible by xml
-     * 
-     * @throws NoSuchFieldException
-     * @throws IllegalArgumentException
-     * @throws IllegalAccessException
-     */
-    private void setTabStripLayoutSpec() {
-        try {
-           
-            // Get text
-            final TextView currTextView = (TextView) mCampaignPagerTabStrip.getChildAt(TAB_CURR_ID);
-            //final TextView nextTextView = (TextView) mCampaignPagerTabStrip.getChildAt(TAB_NEXT_ID);
-            //final TextView prevTextView = (TextView) mCampaignPagerTabStrip.getChildAt(TAB_PREV_ID);
-    
-            // Set Color
-            currTextView.setPadding(0, 0, 0, 1);
-    
-            // Calculate the measures
-            final float density = this.getResources().getDisplayMetrics().density;
-            int mIndicatorHeight = (int) (TAB_INDICATOR_HEIGHT * density + 0.5f);
-            int mFullUnderlineHeight = (int) (TAB_UNDERLINE_HEIGHT * density + 0.5f);
-    
-            // Set the indicator height
-            Field field;
-            field = mCampaignPagerTabStrip.getClass().getDeclaredField("mIndicatorHeight");
-            field.setAccessible(true);
-            field.set(mCampaignPagerTabStrip, mIndicatorHeight);
-            // Set the underline height
-            field = mCampaignPagerTabStrip.getClass().getDeclaredField("mFullUnderlineHeight");
-            field.setAccessible(true);
-            field.set(mCampaignPagerTabStrip, mFullUnderlineHeight);
-            // Set the color of indicator
-            Paint paint = new Paint();
-            paint.setShader(new LinearGradient(0, 0, 0, mIndicatorHeight, 
-                    getResources().getColor(TAB_STRIP_COLOR), 
-                    getResources().getColor(TAB_STRIP_COLOR), 
-                    Shader.TileMode.CLAMP));
-            field = mCampaignPagerTabStrip.getClass().getDeclaredField("mTabPaint");
-            field.setAccessible(true);
-            field.set(mCampaignPagerTabStrip, paint);
-
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        
-    }
-    
 }
