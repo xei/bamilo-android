789a10a6d101ad8b8aa5f2be240f842c69bbf241
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-07-08 08:52:32 +0000

    % Commented STBFragment

--- pt.rocket.view/src/pt/rocket/view/fragments/SlidingTabsBasicFragment.java
+++ pt.rocket.view/src/pt/rocket/view/fragments/SlidingTabsBasicFragment.java
@@ -1,168 +1,168 @@
-package pt.rocket.view.fragments;
-
-import pt.rocket.framework.components.androidslidingtabstrip.SlidingTabLayout;
-import pt.rocket.view.R;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-
-/**
- * A basic sample which shows how to use {@link pt.rocket.framework.components.slidingtabstrip.SlidingTabLayout}
- * to display a custom {@link ViewPager} title strip which gives continuous feedback to the user
- * when scrolling.
- * 
- * http://developer.android.com/downloads/samples/SlidingTabsBasic.zip
- * 
- */
-public class SlidingTabsBasicFragment extends BaseFragment {
-
-    static final String LOG_TAG = "SlidingTabsBasicFragment";
-
-    /**
-     * A custom {@link ViewPager} title strip which looks much like Tabs present in Android v4.0 and
-     * above, but is designed to give continuous feedback to the user when scrolling.
-     */
-    private SlidingTabLayout mSlidingTabLayout;
-
-    /**
-     * A {@link ViewPager} which will be used in conjunction with the {@link SlidingTabLayout} above.
-     */
-    private ViewPager mViewPager;
-    
-    /**
-     * 
-     * @return
-     */
-    public static BaseFragment getInstance() {
-        return new SlidingTabsBasicFragment();
-    }
-
-    /**
-     * Inflates the {@link View} which will be displayed by this {@link Fragment}, from the app's
-     * resources.
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment_main, container, false);
-    }
-
-    // BEGIN_INCLUDE (fragment_onviewcreated)
-    /**
-     * This is called after the {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)} has finished.
-     * Here we can pick out the {@link View}s we need to configure from the content view.
-     *
-     * We set the {@link ViewPager}'s adapter to be an instance of {@link SamplePagerAdapter}. The
-     * {@link SlidingTabLayout} is then given the {@link ViewPager} so that it can populate itself.
-     *
-     * @param view View created in {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
-     */
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // BEGIN_INCLUDE (setup_viewpager)
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
-        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new SamplePagerAdapter());
-        // END_INCLUDE (setup_viewpager)
-
-        // BEGIN_INCLUDE (setup_slidingtablayout)
-        // Give the SlidingTabLayout the ViewPager, this must be done AFTER the ViewPager has had
-        // it's PagerAdapter set.
-        mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
-        mSlidingTabLayout.setViewPager(mViewPager);
-        // END_INCLUDE (setup_slidingtablayout)
-    }
-    // END_INCLUDE (fragment_onviewcreated)
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} used to display pages in this sample.
-     * The individual pages are simple and just display two lines of text. The important section of
-     * this class is the {@link #getPageTitle(int)} method which controls what is displayed in the
-     * {@link SlidingTabLayout}.
-     */
-    class SamplePagerAdapter extends PagerAdapter {
-        
-        private String[] mTabs;
-
-        /**
-         * 
-         */
-        public SamplePagerAdapter() {
-            mTabs = getResources().getStringArray(R.array.tabs_items);
-        }
-
-        /**
-         * @return the number of pages to display
-         */
-        @Override
-        public int getCount() {
-            //return 10;
-            return mTabs.length;
-            
-        }
-
-        /**
-         * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
-         * same object as the {@link View} added to the {@link ViewPager}.
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
-
-        // BEGIN_INCLUDE (pageradapter_getpagetitle)
-        /**
-         * Return the title of the item at {@code position}. This is important as what this method
-         * returns is what is displayed in the {@link SlidingTabLayout}.
-         * <p>
-         * Here we construct one using the position value, but for real application the title should
-         * refer to the item's contents.
-         */
-        @Override
-        public CharSequence getPageTitle(int position) {
-            //return "Item " + (position + 1) ;
-            return mTabs[position] ;
-            
-        }
-        // END_INCLUDE (pageradapter_getpagetitle)
-
-        /**
-         * Instantiate the {@link View} which should be displayed at {@code position}. Here we
-         * inflate a layout from the apps resources and then change the text view to signify the position.
-         */
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            // Inflate a new layout from our resources
-            View view = getActivity().getLayoutInflater().inflate(R.layout.home_fragment_main_item, container, false);
-            // Add the newly created View to the ViewPager
-            container.addView(view);
-
-            // Retrieve a TextView from the inflated View, and update it's text
-            TextView title = (TextView) view.findViewById(R.id.item_title);
-            title.setText(String.valueOf(position + 1));
-
-            //Log.i(LOG_TAG, "instantiateItem() [position: " + position + "]");
-
-            // Return the View
-            return view;
-        }
-
-        /**
-         * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
-         * {@link View}.
-         */
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-            //Log.i(LOG_TAG, "destroyItem() [position: " + position + "]");
-        }
-
-    }
-
-
-}
+//package pt.rocket.view.fragments;
+//
+//import pt.rocket.framework.components.androidslidingtabstrip.SlidingTabLayout;
+//import pt.rocket.view.R;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.view.PagerAdapter;
+//import android.support.v4.view.ViewPager;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//
+///**
+// * A basic sample which shows how to use {@link pt.rocket.framework.components.slidingtabstrip.SlidingTabLayout}
+// * to display a custom {@link ViewPager} title strip which gives continuous feedback to the user
+// * when scrolling.
+// * 
+// * http://developer.android.com/downloads/samples/SlidingTabsBasic.zip
+// * 
+// */
+//public class SlidingTabsBasicFragment extends BaseFragment {
+//
+//    static final String LOG_TAG = "SlidingTabsBasicFragment";
+//
+//    /**
+//     * A custom {@link ViewPager} title strip which looks much like Tabs present in Android v4.0 and
+//     * above, but is designed to give continuous feedback to the user when scrolling.
+//     */
+//    private SlidingTabLayout mSlidingTabLayout;
+//
+//    /**
+//     * A {@link ViewPager} which will be used in conjunction with the {@link SlidingTabLayout} above.
+//     */
+//    private ViewPager mViewPager;
+//    
+//    /**
+//     * 
+//     * @return
+//     */
+//    public static BaseFragment getInstance() {
+//        return new SlidingTabsBasicFragment();
+//    }
+//
+//    /**
+//     * Inflates the {@link View} which will be displayed by this {@link Fragment}, from the app's
+//     * resources.
+//     */
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.home_fragment_main, container, false);
+//    }
+//
+//    // BEGIN_INCLUDE (fragment_onviewcreated)
+//    /**
+//     * This is called after the {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)} has finished.
+//     * Here we can pick out the {@link View}s we need to configure from the content view.
+//     *
+//     * We set the {@link ViewPager}'s adapter to be an instance of {@link SamplePagerAdapter}. The
+//     * {@link SlidingTabLayout} is then given the {@link ViewPager} so that it can populate itself.
+//     *
+//     * @param view View created in {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+//     */
+//    @Override
+//    public void onViewCreated(View view, Bundle savedInstanceState) {
+//        // BEGIN_INCLUDE (setup_viewpager)
+//        // Get the ViewPager and set it's PagerAdapter so that it can display items
+//        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
+//        mViewPager.setAdapter(new SamplePagerAdapter());
+//        // END_INCLUDE (setup_viewpager)
+//
+//        // BEGIN_INCLUDE (setup_slidingtablayout)
+//        // Give the SlidingTabLayout the ViewPager, this must be done AFTER the ViewPager has had
+//        // it's PagerAdapter set.
+//        mSlidingTabLayout = (SlidingTabLayout) view.findViewById(R.id.sliding_tabs);
+//        mSlidingTabLayout.setViewPager(mViewPager);
+//        // END_INCLUDE (setup_slidingtablayout)
+//    }
+//    // END_INCLUDE (fragment_onviewcreated)
+//
+//    /**
+//     * The {@link android.support.v4.view.PagerAdapter} used to display pages in this sample.
+//     * The individual pages are simple and just display two lines of text. The important section of
+//     * this class is the {@link #getPageTitle(int)} method which controls what is displayed in the
+//     * {@link SlidingTabLayout}.
+//     */
+//    class SamplePagerAdapter extends PagerAdapter {
+//        
+//        private String[] mTabs;
+//
+//        /**
+//         * 
+//         */
+//        public SamplePagerAdapter() {
+//            mTabs = getResources().getStringArray(R.array.tabs_items);
+//        }
+//
+//        /**
+//         * @return the number of pages to display
+//         */
+//        @Override
+//        public int getCount() {
+//            //return 10;
+//            return mTabs.length;
+//            
+//        }
+//
+//        /**
+//         * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
+//         * same object as the {@link View} added to the {@link ViewPager}.
+//         */
+//        @Override
+//        public boolean isViewFromObject(View view, Object o) {
+//            return o == view;
+//        }
+//
+//        // BEGIN_INCLUDE (pageradapter_getpagetitle)
+//        /**
+//         * Return the title of the item at {@code position}. This is important as what this method
+//         * returns is what is displayed in the {@link SlidingTabLayout}.
+//         * <p>
+//         * Here we construct one using the position value, but for real application the title should
+//         * refer to the item's contents.
+//         */
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            //return "Item " + (position + 1) ;
+//            return mTabs[position] ;
+//            
+//        }
+//        // END_INCLUDE (pageradapter_getpagetitle)
+//
+//        /**
+//         * Instantiate the {@link View} which should be displayed at {@code position}. Here we
+//         * inflate a layout from the apps resources and then change the text view to signify the position.
+//         */
+//        @Override
+//        public Object instantiateItem(ViewGroup container, int position) {
+//            // Inflate a new layout from our resources
+//            View view = getActivity().getLayoutInflater().inflate(R.layout.home_fragment_main_item, container, false);
+//            // Add the newly created View to the ViewPager
+//            container.addView(view);
+//
+//            // Retrieve a TextView from the inflated View, and update it's text
+//            TextView title = (TextView) view.findViewById(R.id.item_title);
+//            title.setText(String.valueOf(position + 1));
+//
+//            //Log.i(LOG_TAG, "instantiateItem() [position: " + position + "]");
+//
+//            // Return the View
+//            return view;
+//        }
+//
+//        /**
+//         * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
+//         * {@link View}.
+//         */
+//        @Override
+//        public void destroyItem(ViewGroup container, int position, Object object) {
+//            container.removeView((View) object);
+//            //Log.i(LOG_TAG, "destroyItem() [position: " + position + "]");
+//        }
+//
+//    }
+//
+//
+//}
