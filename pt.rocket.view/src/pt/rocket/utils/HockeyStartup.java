6c87979cf35671f6c91c68c141348cd78547a857
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-12-01 17:37:12 +0000

    % Updated Hockey to show dialog for dev.

--- pt.rocket.view/src/pt/rocket/utils/HockeyStartup.java
+++ pt.rocket.view/src/pt/rocket/utils/HockeyStartup.java
@@ -83,7 +83,7 @@ public class HockeyStartup {
             Log.d(TAG, "start: starting CrashManager" );
             CrashManager.register(activity, hockeyTocken, sCml);
         }
-        if (resultCheckSignature == RESULT_KEY_HOCKEY) {
+        if (resultCheckSignature == RESULT_KEY_HOCKEY || enableDevVersion) {
             Log.d( TAG, "start: starting UpdateManager" );
             UpdateManager.register(activity, hockeyTocken, sUml);
         }
