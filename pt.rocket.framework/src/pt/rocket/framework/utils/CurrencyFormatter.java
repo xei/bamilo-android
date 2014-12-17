2b40ee2efde90093ba73148c47eed7d8ae56d753
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2014-12-17 19:31:55 +0000

    % Updated the currency formatter to use a default formatter.

--- pt.rocket.framework/src/pt/rocket/framework/utils/CurrencyFormatter.java
+++ pt.rocket.framework/src/pt/rocket/framework/utils/CurrencyFormatter.java
@@ -20,6 +20,7 @@ import de.akquinet.android.androlog.Log;
  *
  */
 public class CurrencyFormatter {
+    
 	private final static String TAG = LogTagHelper.create( CurrencyFormatter.class );
 	
 	public final static String EURO_CODE = "EUR";
@@ -46,150 +47,53 @@ public class CurrencyFormatter {
      * Initializes the Currency formatter.
      * This function accesses the content data in order to access the currency language and country of the currency.
      */
-    public static void initialize( Context context, String currCode) {
+    public static void initialize(Context context, String currCode) {
     	currencyCode = currCode;
         apiLocale = Locale.US;
         initialized = true;
-        isLocaleAvailable = isLocaleAvailable( Locale.getDefault());
-        loadCurrencyInformation( context, currCode );
+        isLocaleAvailable = isLocaleAvailable(Locale.getDefault());
+        // Load currency configurations
+        loadCurrencyInformation(context, currCode);
         
-        if ( TextUtils.isEmpty( currCode ))
-        	throw new RuntimeException( "Currency code is empty or null - fix this" );
+        if (TextUtils.isEmpty(currCode)) throw new RuntimeException("Currency code is empty or null - fix this");
         
-        
-        if(currCode.equalsIgnoreCase("USH")){
-        	currCode = "UGX";
-        }
-        
-        // TODO: BAMILO TEST
-        if(currCode.equalsIgnoreCase("ريال")) currCode = "IRR";
+        // Currency fallbacks
+        // Case Uganda (Jumia)
+        if(currCode.equalsIgnoreCase("USH"))currCode = "UGX";
+        // Case Iran (Bamilo)
+        else if(currCode.equalsIgnoreCase("ريال")) currCode = "IRR";
         
         currency = Currency.getInstance(currCode);
-        Log.d( TAG, "currency: currency code = " + currency.getCurrencyCode() + " fraction dicits = " + currency.getDefaultFractionDigits());
         formatter = getNumberFormatter();
+        
+        Log.i( TAG, "CURRENCY: currency code = " + currency.getCurrencyCode() + " fraction dicits = " + currency.getDefaultFractionDigits());
     }
     
-    public static void loadCurrencyInformation( Context context, String currCode ) {
+    /**
+     * Validate locale is available.
+     * @param localeRequired
+     * @return true or false
+     */
+    private static boolean isLocaleAvailable(Locale localeRequired) {
+        for (Locale loc : DecimalFormat.getAvailableLocales()) {
+            if (loc.getCountry().equalsIgnoreCase(localeRequired.getCountry())) return true;
+        }
+        return false;
+    }
+    
+    
+    /**
+     * Load currency info from country configurations.
+     * @param context
+     * @param currCode
+     * @modified sergiopereira
+     */
+    public static void loadCurrencyInformation(Context context, String currCode) {
     	SharedPreferences sharedPrefs = context.getSharedPreferences(Darwin.SHARED_PREFERENCES, Context.MODE_PRIVATE);
-    	
-
     	currencyThousandsDelim = sharedPrefs.getString(Darwin.KEY_SELECTED_COUNTRY_THOUSANDS_SEP, ",");
     	currencyFractionCount = sharedPrefs.getInt(Darwin.KEY_SELECTED_COUNTRY_NO_DECIMALS, 0);
     	currencyFractionDelim = sharedPrefs.getString(Darwin.KEY_SELECTED_COUNTRY_DECIMALS_SEP, ".");
-    	
     	currencyUnitPattern = sharedPrefs.getString(Darwin.KEY_SELECTED_COUNTRY_CURRENCY_SYMBOL, ".");
-    }
-    
-    
-    
-    public static void setCurrency( String currencyCode ) {
-    	currency = Currency.getInstance(currencyCode);
-    }
-    
-//    /**
-//     * Formats the currency based on the pre-defined values.
-//     * @param value
-//     * @return the formatted string
-//     */
-//    public static String formatCurrency(double value) {
-//    	if (!initialized) throw new RuntimeException("currency converter not initialized");
-//    	
-//    	// String result = String.valueOf(value);
-//    	String result = new BigDecimal(value).toString();
-//
-//    	String noFraction;
-//    	String fraction;
-//    	if(result.contains(".")){
-//    		StringTokenizer tokens = new StringTokenizer(result, ".");
-//    		noFraction = tokens.nextToken();
-//    		fraction = tokens.nextToken();
-//    	} else {
-//    		noFraction = result;
-//    		fraction =null;
-//    	}
-//    	
-//    	if(noFraction.length()>3){
-//	    	String thousands = noFraction.substring(noFraction.length()-3, noFraction.length());
-//	    	String other = noFraction.substring(0, noFraction.length()-3);
-//	    
-//	    	if(currencyFractionCount == 0)
-//	    		result = other+currencyThousandsDelim+thousands;
-//	    	else { 
-//	    		
-//	    		while(fraction.length()<currencyFractionCount){
-//	    			fraction+="0";
-//	    		}
-//	    		result = other+currencyThousandsDelim+thousands+currencyFractionDelim+fraction;
-//	    	}
-//    	} else {
-//    		
-//    		if(currencyFractionCount == 0)
-//    			result = noFraction;
-//	    	else { 
-//	    		while(fraction.length()<currencyFractionCount){
-//	    			fraction+="0";
-//	    		}
-//	    		
-//	    		result = noFraction+currencyFractionDelim+fraction;
-//	    	}
-//    		
-//    	}
-//
-//    	result = String.format(currencyUnitPattern, result);
-//    	
-//    	return result;
-//    }
-    
-    /**
-     * This Function restrieves the double value of a previously formatted value with currency and locale options.
-     * @param value
-     * @return
-     */
-    public static double getValueDouble(String value ) {
-    	double result = 0;
-    	if(currencyFractionCount == 0)
-    		value = value.replace(",", "");
-    	else { 
-    		if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && !value.contains(".")){
-    			value = value.replace(",", ".");
-    		} else if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && value.contains(".")) {
-    			value = value.replace(".", "");
-    			value = value.replace(",", ".");
-    		} else if(value.contains(",")){
-    			value = value.replace(",", "");
-    		}
-    	}
-    	try {
-    		result = Double.parseDouble(value);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	
-    	return result;
-    }
-    
-    /**
-     * This Function restrieves the double value of a previously formatted value with currency and locale options.
-     * @param value
-     * @return
-     */
-    public static String getCleanValue(String value ) {
-    	value = value.trim();
-    	String result = "";
-    	if(currencyFractionCount == 0)
-    		value = value.replace(",", "");
-    	else { 
-    		if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && !value.contains(".")){
-    			value = value.replace(",", ".");
-    		} else if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && value.contains(".")) {
-    			value = value.replace(".", "");
-    			value = value.replace(",", ".");
-    		} else if(value.contains(",")){
-    			value = value.replace(",", "");
-    		}
-    	}
-    	result = value;
-    	return result;
     }
     
     /**
@@ -206,13 +110,11 @@ public class CurrencyFormatter {
             
             NumberFormat format = NumberFormat.getInstance(apiLocale);
             Number number = format.parse(value);
-
-            Double valueDouble = number.doubleValue();            
-        	return String.format( currencyUnitPattern, formatter.format(valueDouble));
-
-        
+            Double valueDouble = number.doubleValue();
+        	return String.format(currencyUnitPattern, formatter.format(valueDouble));
+        	
         } catch (NumberFormatException e) {
-            //In case of bad formatting, return the parsed value with no crrency sign
+            //In case of bad formatting, return the parsed value with no currency sign
         	Log.e( TAG, "bad formatting for value = " + value, e );
             return value + "";
         } catch (ParseException e) {
@@ -221,38 +123,58 @@ public class CurrencyFormatter {
         }
     }
     
+    /**
+     * Get the currency formatter.
+     * @return NumberFormat
+     */
     private static NumberFormat getNumberFormatter() {
         if (!initialized) throw new RuntimeException("currency converter not initialized");
     	
-    	NumberFormat currencyFormat;
-    	if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
-    		currencyFormat = createNumberFormatter();
-    	} else {
-    		currencyFormat = DecimalFormat.getCurrencyInstance();
-    	}
+        // Get the number format the according with Locale value 
+    	//NumberFormat currencyFormat = getNumberFormat();
+        // Get the universal number format
+        NumberFormat currencyFormat = DecimalFormat.getCurrencyInstance(Locale.US);
     	
-        currencyFormat.setMaximumFractionDigits( currencyFractionCount );
+        currencyFormat.setMaximumFractionDigits(currencyFractionCount);
         currencyFormat.setMinimumFractionDigits(currencyFractionCount);
         currencyFormat.setGroupingUsed(true);
         DecimalFormatSymbols dfs = ((DecimalFormat) currencyFormat).getDecimalFormatSymbols();
-        dfs.setCurrencySymbol( "" );
+        dfs.setCurrencySymbol("");
         dfs.setGroupingSeparator(currencyThousandsDelim.charAt(0));
-        // dfs.setGroupingSeparator(',');
-        if ( currencyFractionDelim != null) {
+        if (currencyFractionDelim != null) {
         	dfs.setMonetaryDecimalSeparator(currencyFractionDelim.charAt(0));
         }
-        ((DecimalFormat)currencyFormat).setDecimalFormatSymbols( dfs );
+        ((DecimalFormat)currencyFormat).setDecimalFormatSymbols(dfs);
     	        
         return currencyFormat;
     }
     
+    
+    /**
+     * Get the number format.<br>
+     * Case build version <= 2.x returns the universal number format (Locale.US).<br>
+     * Otherwise returns the number format for the current Locale.<br>
+     * @return NumberFormat
+     * @author GuilhermeSilva
+     * @modified sergiopereira
+     */
+    @SuppressWarnings("unused")
+    private static NumberFormat getNumberFormat() {
+        return Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1 ? createNumberFormatter() : DecimalFormat.getCurrencyInstance();
+    }
+    
+    /**
+     * Creates the number format for devices 2.x.
+     * @return NumberFormat
+     * @author GuilhermeSilva
+     */
 	private static NumberFormat createNumberFormatter() {
-		Log.d( TAG, "createNumberFormatter for android 2.x");
+		//Log.d( TAG, "createNumberFormatter for android 2.x");
 		NumberFormat formatter;
 		Locale requiredLocale = Locale.getDefault();
 		Locale formatterLocale;
 		if (!isLocaleAvailable) {
-			Log.d( TAG, "required locale " + requiredLocale.getCountry() + " is not available - fallback to backup");
+			//Log.d( TAG, "required locale " + requiredLocale.getCountry() + " is not available - fallback to backup");
 			if (requiredLocale.getCountry().equals("ID") || requiredLocale.getCountry().equals("PH")) {
 				formatterLocale = Locale.US;
 			} else {
@@ -266,22 +188,15 @@ public class CurrencyFormatter {
 		return formatter;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public static String getCurrencyCode() {
     	return currencyCode;
     }
     
-    public static boolean isLocaleAvailable( Locale localeRequired) {
-    	Locale[] availableLocales = DecimalFormat.getAvailableLocales();
-    	// Log.d( TAG, "isLocaleAvailable country = " + localeRequired + " available locale number = " + availableLocales.length);
-    	for( Locale loc: availableLocales) {
-			// Log.d( TAG, "localeRequired = " + localeRequired.getCountry() + " crrLocale = " + loc.getCountry());
-    		if ( loc.getCountry().equalsIgnoreCase( localeRequired.getCountry())) {
-    			return true;
-    		}
-    	}
-    	
-    	return false;
-    }
+
 
     /**
      * Test if text is number
@@ -297,4 +212,117 @@ public class CurrencyFormatter {
             return false;
         }
     }
+    
+    
+//  public static void setCurrency( String currencyCode ) {
+//  currency = Currency.getInstance(currencyCode);
+//}
+
+///**
+// * Formats the currency based on the pre-defined values.
+// * @param value
+// * @return the formatted string
+// */
+//public static String formatCurrency(double value) {
+//  if (!initialized) throw new RuntimeException("currency converter not initialized");
+//  
+//  // String result = String.valueOf(value);
+//  String result = new BigDecimal(value).toString();
+//
+//  String noFraction;
+//  String fraction;
+//  if(result.contains(".")){
+//      StringTokenizer tokens = new StringTokenizer(result, ".");
+//      noFraction = tokens.nextToken();
+//      fraction = tokens.nextToken();
+//  } else {
+//      noFraction = result;
+//      fraction =null;
+//  }
+//  
+//  if(noFraction.length()>3){
+//      String thousands = noFraction.substring(noFraction.length()-3, noFraction.length());
+//      String other = noFraction.substring(0, noFraction.length()-3);
+//  
+//      if(currencyFractionCount == 0)
+//          result = other+currencyThousandsDelim+thousands;
+//      else { 
+//          
+//          while(fraction.length()<currencyFractionCount){
+//              fraction+="0";
+//          }
+//          result = other+currencyThousandsDelim+thousands+currencyFractionDelim+fraction;
+//      }
+//  } else {
+//      
+//      if(currencyFractionCount == 0)
+//          result = noFraction;
+//      else { 
+//          while(fraction.length()<currencyFractionCount){
+//              fraction+="0";
+//          }
+//          
+//          result = noFraction+currencyFractionDelim+fraction;
+//      }
+//      
+//  }
+//
+//  result = String.format(currencyUnitPattern, result);
+//  
+//  return result;
+//}
+
+///**
+// * This Function restrieves the double value of a previously formatted value with currency and locale options.
+// * @param value
+// * @return
+// */
+//public static double getValueDouble(String value ) {
+//  double result = 0;
+//  if(currencyFractionCount == 0)
+//      value = value.replace(",", "");
+//  else { 
+//      if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && !value.contains(".")){
+//          value = value.replace(",", ".");
+//      } else if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && value.contains(".")) {
+//          value = value.replace(".", "");
+//          value = value.replace(",", ".");
+//      } else if(value.contains(",")){
+//          value = value.replace(",", "");
+//      }
+//  }
+//  try {
+//      result = Double.parseDouble(value);
+//  } catch (Exception e) {
+//      e.printStackTrace();
+//  }
+//  
+//  return result;
+//}
+
+///**
+// * This Function restrieves the double value of a previously formatted value with currency and locale options.
+// * @param value
+// * @return
+// */
+//public static String getCleanValue(String value ) {
+//  value = value.trim();
+//  String result = "";
+//  if(currencyFractionCount == 0)
+//      value = value.replace(",", "");
+//  else { 
+//      if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && !value.contains(".")){
+//          value = value.replace(",", ".");
+//      } else if(value.contains(",") && (""+currencyFractionDelim).equalsIgnoreCase(",") && value.contains(".")) {
+//          value = value.replace(".", "");
+//          value = value.replace(",", ".");
+//      } else if(value.contains(",")){
+//          value = value.replace(",", "");
+//      }
+//  }
+//  result = value;
+//  return result;
+//}
+    
+    
 }
