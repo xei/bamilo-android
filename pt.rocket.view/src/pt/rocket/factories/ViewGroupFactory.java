4e38e9268fd81442cb55da95a2b92f1a0777d3ff
ricardo.soares <ricardo.soares@africainternetgroup.com>
2015-02-06 17:09:59 +0000

    % Bug fix: App is no longer crashing on tablet. NAFAMZ-10179

--- pt.rocket.view/src/pt/rocket/factories/ViewGroupFactory.java
+++ pt.rocket.view/src/pt/rocket/factories/ViewGroupFactory.java
@@ -1,36 +1,40 @@
 package pt.rocket.factories;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ViewGroupFactory{
+public class ViewGroupFactory {
     // TODO validate if it's better wih SparceArray
     private HashMap<Integer, View> views;
-    
-    public ViewGroupFactory(ViewGroup viewGroup){
+
+    public ViewGroupFactory(ViewGroup viewGroup) {
         views = new HashMap<Integer, View>();
-        for(int i = 0; i<viewGroup.getChildCount();i++){
-            View child = viewGroup.getChildAt(i); 
-            views.put(child.getId(), child);
+        if (viewGroup != null) {
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                views.put(child.getId(), child);
+            }
         }
     }
-    
-    public ViewGroupFactory(View... args){
-        for(View child : args){
-            views.put(child.getId(), child);
+
+    public ViewGroupFactory(View... args) {
+        views = new HashMap<Integer, View>();
+        for (View child : args) {
+            if (child != null) {
+                views.put(child.getId(), child);
+            }
         }
     }
-    
-    public View setViewVisible(int id){
+
+    public View setViewVisible(int id) {
         View child = null;
-        for(Entry<Integer, View> entry : views.entrySet()) {
+        for (Entry<Integer, View> entry : views.entrySet()) {
             int key = entry.getKey();
             View value = entry.getValue();
-            if(key == id){
+            if (key == id) {
                 value.setVisibility(View.VISIBLE);
                 child = value;
             } else {
@@ -39,16 +43,15 @@ public class ViewGroupFactory{
         }
         return child;
     }
-    
-    public View getVisibleView(){
-        for(Entry<Integer, View> entry : views.entrySet()) {
+
+    public View getVisibleView() {
+        for (Entry<Integer, View> entry : views.entrySet()) {
             View value = entry.getValue();
-            if(value.getVisibility() == View.VISIBLE){
+            if (value.getVisibility() == View.VISIBLE) {
                 return value;
             }
         }
         return null;
     }
-    
-    
+
 }
