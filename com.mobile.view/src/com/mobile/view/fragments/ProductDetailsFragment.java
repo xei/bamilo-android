fd7e0c666b43aa7af808087761107923933ad590
sergiopereira-aig <sergio.pereira@africainternetgroup.com>
2015-02-10 11:54:25 +0000

    % Updated the layout for CheckoutThx, NAFAMZ-10234.

--- com.mobile.view/src/com/mobile/view/fragments/ProductDetailsFragment.java
+++ com.mobile.view/src/com/mobile/view/fragments/ProductDetailsFragment.java
@@ -22,7 +22,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.text.Html;
 import android.text.Spannable;
 import android.text.TextUtils;
