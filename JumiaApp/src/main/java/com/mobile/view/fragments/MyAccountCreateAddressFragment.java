95760b6dd0354404c63b09960cd1a3d18cdcc295
Ricardo Soares <ricardo.soares@africainternetgroup.com>
2015-04-06 15:21:35 +0100

    % Create address added to app's flow when user doesn't have addresses.

--- JumiaApp/src/main/java/com/mobile/view/fragments/MyAccountCreateAddressFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/MyAccountCreateAddressFragment.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.mobile.app.JumiaApplication;
 import com.mobile.components.customfontviews.Button;
+import com.mobile.controllers.fragments.FragmentController;
+import com.mobile.controllers.fragments.FragmentType;
 import com.mobile.framework.ErrorCode;
 import com.mobile.framework.tracking.AnalyticsGoogle;
 import com.mobile.framework.tracking.TrackingEvent;
@@ -15,6 +17,7 @@ import com.mobile.framework.utils.LogTagHelper;
 import com.mobile.utils.MyMenuItem;
 import com.mobile.utils.NavigationAction;
 import com.mobile.utils.Toast;
+import com.mobile.utils.TrackerDelegator;
 import com.mobile.view.R;
 
 import java.util.EnumSet;
@@ -33,18 +36,24 @@ public class MyAccountCreateAddressFragment extends CreateAddressFragment {
 
     private static final String TAG = LogTagHelper.create(MyAccountCreateAddressFragment.class);
 
+    protected boolean firstUserAddress;
+
     /**
      * Fragment used to create an address
-     * @return CheckoutCreateAddressFragment
-     * @author sergiopereira
+     * @return MyAccountCreateAddressFragment
+     *
      */
-    public static MyAccountCreateAddressFragment newInstance() {
-        return new MyAccountCreateAddressFragment();
+    public static MyAccountCreateAddressFragment newInstance(Bundle bundle) {
+        MyAccountCreateAddressFragment myAccountCreateAddressFragment = new MyAccountCreateAddressFragment();
+        if(bundle != null) {
+            myAccountCreateAddressFragment.firstUserAddress = bundle.getBoolean(TrackerDelegator.LOGIN_KEY, false);
+        }
+        return myAccountCreateAddressFragment;
     }
 
     /**
      * Empty constructor
-     * @author sergiopereira
+     *
      */
     public MyAccountCreateAddressFragment() {
         super(EnumSet.of(MyMenuItem.UP_BUTTON_BACK, MyMenuItem.SEARCH_VIEW, MyMenuItem.BASKET, MyMenuItem.MY_PROFILE),
@@ -98,9 +107,14 @@ public class MyAccountCreateAddressFragment extends CreateAddressFragment {
                 ContentValues mBillValues = createContentValues(billingFormGenerator, ISNT_DEFAULT_SHIPPING_ADDRESS, IS_DEFAULT_BILLING_ADDRESS);
                 triggerCreateAddress(mBillValues, true);
             }
+        } else {
+            if(firstUserAddress){
+                FragmentController.getInstance().popLastEntry(FragmentType.MY_ACCOUNT_CREATE_ADDRESS.toString());
+                getBaseActivity().onSwitchFragment(FragmentType.MY_ACCOUNT_MY_ADDRESSES, null, true);
+            } else {
+                getBaseActivity().onBackPressed();
+            }
         }
-
-        getBaseActivity().onBackPressed();
     }
 
     @Override
