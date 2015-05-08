428b1f59515c5560df5d7a74de27fe956167a617
Paulo Carvalho <paulo.carvalho@africainternetgroup.com>
2015-05-08 17:13:36 +0100

    removed unused code;

--- JumiaApp/src/main/java/com/mobile/view/fragments/CheckoutPaymentMethodsFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/CheckoutPaymentMethodsFragment.java
@@ -259,15 +259,6 @@ public class CheckoutPaymentMethodsFragment extends BaseFragment implements IRes
         paymentMethodsContainer.addView(formGenerator.getContainer());
         loadSavedValues(mSavedState, formGenerator.getIterator());
         paymentMethodsContainer.refreshDrawableState();
-        // Try request focus for default selection
-        try {
-            if (formGenerator.getControlsCount() == 1) {
-                int selected = ((RadioGroupLayoutVertical) formGenerator.getItem(0).getEditControl()).getSelectedIndex();
-                ((RadioGroupLayoutVertical) formGenerator.getItem(0).getEditControl()).getChildAt(selected).requestFocus();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         prepareCouponView();
         showFragmentContentContainer();
     }
