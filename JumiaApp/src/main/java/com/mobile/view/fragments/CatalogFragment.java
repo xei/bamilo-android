2fe51c2ecf71ca3b6e183a649b8c63a26c487ed7
sergio pereira <sergio.pereira@africainternetgroup.com>
2015-05-08 17:57:04 +0100

    NAFAMZ-11803 - Android :: Wrong behavior when is applied a brand filter

--- JumiaApp/src/main/java/com/mobile/view/fragments/CatalogFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/CatalogFragment.java
@@ -600,8 +600,6 @@ public class CatalogFragment extends BaseFragment implements IResponseCallback, 
         if (filterValues.containsKey(DialogFilterFragment.BRAND)) {
             // Used to indicate that has filter q=<BRAND>
             mBrandQuery = filterValues.getAsString(DialogFilterFragment.BRAND);
-            // Remove brand from values (API: not supported)
-            filterValues.remove(DialogFilterFragment.BRAND);
         }
         // Clean brand filter
         else {
