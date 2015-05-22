1cc5452f0d1ea6e2a53df425f05cdad721aa7654
Ricardo Soares <ricardo.soares@africainternetgroup.com>
2015-05-22 10:19:03 +0100

    + Created Metadata and changed BaseResponse.

--- libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/pojo/BaseResponse.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/pojo/BaseResponse.java
@@ -1,11 +1,18 @@
 package com.mobile.newFramework.pojo;
 
+import java.util.List;
+
 /**
  * Created by spereira on 5/19/15.
  */
-public class BaseResponse {
-
+public class BaseResponse<T>{
     public String success;
+    public List<String> messages;
+    public Metadata<T> metadata;
+
+    public BaseResponse(){
+        metadata = new Metadata<T>();
+    }
 
 }
 
