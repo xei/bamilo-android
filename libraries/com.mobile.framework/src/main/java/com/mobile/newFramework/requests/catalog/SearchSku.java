4ee67f159231260b082d481ab1f4f95f583cf770
paulocarvalhoaig <paulo.carvalho@africainternetgroup.com>
2015-06-02 11:47:07 +0100

    Added Search sku test and request;

---
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/requests/catalog/SearchSku.java
@@ -0,0 +1,23 @@
+package com.mobile.newFramework.requests.catalog;
+
+import android.content.Context;
+
+import com.mobile.newFramework.interfaces.AigApiInterface;
+import com.mobile.newFramework.interfaces.AigResponseCallback;
+import com.mobile.newFramework.objects.product.CompleteProduct;
+import com.mobile.newFramework.requests.BaseRequest;
+import com.mobile.newFramework.requests.RequestBundle;
+import com.mobile.newFramework.rest.AigRestAdapter;
+
+public class SearchSku extends BaseRequest<CompleteProduct> {
+
+    public SearchSku(Context context, RequestBundle requestBundle, AigResponseCallback requester) {
+        super(context, requestBundle, requester);
+    }
+
+    @Override
+    public void execute() {
+        AigApiInterface service = AigRestAdapter.getRestAdapter(mContext, mRequestBundle.toRestAdapterInit()).create(AigApiInterface.class);
+        service.searchSku(mRequestBundle.getData(), this);
+    }
+}
