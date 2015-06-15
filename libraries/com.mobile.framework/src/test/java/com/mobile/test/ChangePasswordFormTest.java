8f57f826ad79602986b0b8117bf73fd0ac528d85
Ricardo Soares <ricardo.soares@africainternetgroup.com>
2015-06-15 11:32:02 +0100

    % Created GetChangePassword helper.

--- libraries/com.mobile.framework/src/test/java/com/mobile/test/ChangePasswordFormTest.java
+++ libraries/com.mobile.framework/src/test/java/com/mobile/test/ChangePasswordFormTest.java
@@ -6,7 +6,6 @@ import com.mobile.newFramework.pojo.BaseResponse;
 import com.mobile.newFramework.requests.BaseRequest;
 import com.mobile.newFramework.requests.RequestBundle;
 import com.mobile.newFramework.rest.interfaces.AigApiInterface;
-import com.mobile.newFramework.rest.interfaces.AigResponseCallback;
 import com.mobile.newFramework.utils.EventType;
 import com.mobile.newFramework.utils.output.Print;
 
