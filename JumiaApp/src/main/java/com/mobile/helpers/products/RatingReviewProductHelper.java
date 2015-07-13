f590080b6ad7f0df6bef1e18ca014cc9c70ee095
sergio pereira <sergio.pereira@africainternetgroup.com>
2015-07-13 14:28:24 +0100

    % Merge from dev.

--- JumiaApp/src/main/java/com/mobile/helpers/products/RatingReviewProductHelper.java
+++ JumiaApp/src/main/java/com/mobile/helpers/products/RatingReviewProductHelper.java
@@ -4,14 +4,12 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.mobile.helpers.SuperBaseHelper;
-import com.mobile.newFramework.pojo.BaseResponse;
 import com.mobile.newFramework.requests.BaseRequest;
 import com.mobile.newFramework.requests.RequestBundle;
 import com.mobile.newFramework.rest.RestUrlUtils;
 import com.mobile.newFramework.rest.interfaces.AigApiInterface;
 import com.mobile.newFramework.utils.EventTask;
 import com.mobile.newFramework.utils.EventType;
-import com.mobile.newFramework.utils.output.Print;
 
 /**
  * Send Rating and/or Review to API
