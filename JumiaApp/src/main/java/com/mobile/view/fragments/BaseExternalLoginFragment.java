351104d31bdec911c69f7133987102c828d56be7
Ricardo Soares <ricardo.soares@africainternetgroup.com>
2015-07-28 13:06:12 +0100

    % Bug fix: App isn't crashing anymore when is required permissions from facebook (login).

--- JumiaApp/src/main/java/com/mobile/view/fragments/BaseExternalLoginFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/BaseExternalLoginFragment.java
@@ -14,6 +14,7 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.mobile.newFramework.utils.CustomerUtils;
 import com.mobile.newFramework.utils.NetworkConnectivity;
+import com.mobile.newFramework.utils.output.Print;
 import com.mobile.utils.MyMenuItem;
 import com.mobile.utils.NavigationAction;
 import com.mobile.utils.Toast;
@@ -129,8 +130,15 @@ public abstract class BaseExternalLoginFragment extends BaseFragment implements 
      * Arrays.asList(FacebookHelper.FB_PERMISSION_EMAIL)
      */
     public void repeatFacebookEmailRequest(){
-        showFragmentLoading();
-        LoginManager.getInstance().logInWithReadPermissions(this, Collections.singletonList(FacebookHelper.FB_PERMISSION_EMAIL));
+
+        if(!isOnStoppingProcess) {
+            showFragmentLoading();
+            try {
+                LoginManager.getInstance().logInWithReadPermissions(this, Collections.singletonList(FacebookHelper.FB_PERMISSION_EMAIL));
+            } catch (NullPointerException ex) {
+                Print.e(TAG, "Error trying to launch facebook dialog for requesting permissions", ex);
+            }
+        }
     }
 
     /*
@@ -144,7 +152,13 @@ public abstract class BaseExternalLoginFragment extends BaseFragment implements 
         // Validate required permission
         String email = user.optString(FacebookHelper.FACEBOOK_EMAIL_TAG);
         if (TextUtils.isEmpty(email)) {
-            repeatFacebookEmailRequest();
+            this.getView().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    repeatFacebookEmailRequest();
+                }
+            },5000);
+
             return;
         }
         // Trigger
