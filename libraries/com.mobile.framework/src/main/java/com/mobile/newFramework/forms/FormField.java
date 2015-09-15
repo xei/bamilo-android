759b3afae1f9ecf925e6c649e1e4668ac85552ae
paulocarvalhoaig <paulo.carvalho@africainternetgroup.com>
2015-09-15 17:56:10 +0100

    % corrected the gender flag;

--- libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/forms/FormField.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/forms/FormField.java
@@ -228,7 +228,7 @@ public class FormField implements IJSONSerializable, IFormField, Parcelable {
                     // Case default
                     else {
                         JSONObject option = dataOptionsArray.getJSONObject(i);
-                        dataSet.put(option.optString("label"), option.optString("value"));
+                        dataSet.put(option.optString("value"), option.optString("label"));
                     }
                 }
             }
