ad3632f6307216d0619e5142039997b0e440ea4d
alexandrapires <alexandra.pires@africainternetgroup.com>
2015-09-23 11:46:07 +0100

    %changed layout definition

--- JumiaApp/src/main/java/com/mobile/view/fragments/ProductDetailsSummaryFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/ProductDetailsSummaryFragment.java
@@ -67,7 +67,7 @@ public class ProductDetailsSummaryFragment extends BaseFragment {
         super(EnumSet.of(MyMenuItem.UP_BUTTON_BACK, MyMenuItem.SEARCH_VIEW, MyMenuItem.BASKET, MyMenuItem.MY_PROFILE),
                 NavigationAction.Product,
                 //      R.layout.product_summary_fragment,
-                R.layout._def_product_summary_fragment_new,
+                R.layout.product_summary_fragment_new,
                 NO_TITLE,
                 KeyboardState.NO_ADJUST_CONTENT);
     }
