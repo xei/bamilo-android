f9c67a63aef00d31bbb0e33f57c9ce6911872b41
Ricardo Soares <ricardo.soares@africainternetgroup.com>
2015-10-14 16:06:33 +0100

    Merge branches 'NAFAMZ-13628_WebView_XSS' and 'development' of https://github.com/SILVEROAKLDA/JUMIAAND into NAFAMZ-13628_WebView_XSS
    Conflicts:
    	JumiaApp/src/main/java/com/mobile/view/fragments/CheckoutExternalPaymentFragment.java

--- JumiaApp/src/main/java/com/mobile/view/fragments/CheckoutExternalPaymentFragment.java
+++ JumiaApp/src/main/java/com/mobile/view/fragments/CheckoutExternalPaymentFragment.java
@@ -38,7 +38,6 @@ import com.mobile.newFramework.utils.output.Print;
 import com.mobile.utils.HockeyStartup;
 import com.mobile.utils.MyMenuItem;
 import com.mobile.utils.NavigationAction;
-import com.mobile.utils.Toast;
 import com.mobile.utils.TrackerDelegator;
 import com.mobile.utils.ui.ToastManager;
 import com.mobile.view.R;
