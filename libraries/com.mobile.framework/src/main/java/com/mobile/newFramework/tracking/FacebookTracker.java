a4fe992c1263a55e057290fa6e1763433b8a0b31
paulocarvalhoaig <paulo.carvalho@africainternetgroup.com>
2015-10-30 14:06:50 +0000

    % commented facebook tracker implementation;

--- libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/tracking/FacebookTracker.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/tracking/FacebookTracker.java
@@ -144,7 +144,7 @@ public class FacebookTracker {
      */
     public void trackAddedToCart(String sku, double price, String shopCountry, String appVersion) {
         Log.i(TAG, "TRACK ADD TO CART: " + sku + " " + price);
-        trackItem(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, sku, price, shopCountry, appVersion);
+//        trackItem(AppEventsConstants.EVENT_NAME_ADDED_TO_CART, sku, price, shopCountry, appVersion);
     }
 
     /**
@@ -155,11 +155,11 @@ public class FacebookTracker {
      */
     public void trackCatalogView(String category, String shopCountry, String appVersion) {
         Log.i(TAG, "TRACK CATALOG VIEW: " + category);
-        Bundle params = new Bundle();
-        params.putString(EVENT_PARAM_CONTENT_CATEGORY, category);
-        params.putString(EVENT_PARAM_SHOP_COUNTRY, shopCountry);
-        params.putString(EVENT_PARAM_APP_VERSION, appVersion);
-        logEvent(EVENT_VIEW_CONTENT_CATEGORY, params);
+//        Bundle params = new Bundle();
+//        params.putString(EVENT_PARAM_CONTENT_CATEGORY, category);
+//        params.putString(EVENT_PARAM_SHOP_COUNTRY, shopCountry);
+//        params.putString(EVENT_PARAM_APP_VERSION, appVersion);
+//        logEvent(EVENT_VIEW_CONTENT_CATEGORY, params);
     }
 
     /**
@@ -172,8 +172,7 @@ public class FacebookTracker {
      */
     public void trackProduct(String sku, double price, String shopCountry, String appVersion) {
         Log.i(TAG, "TRACK PRODUCT DETAIL: " + sku + " " + price);
-//      trackItem(AppEventsConstants.EVENT_NAME_VIEWED_CONTENT, sku, price, shopCountry, appVersion);
-        trackItem(EVENT_VIEW_CONTENT_PRODUCT, sku, price, shopCountry, appVersion);
+//        trackItem(EVENT_VIEW_CONTENT_PRODUCT, sku, price, shopCountry, appVersion);
     }
 
 
@@ -189,76 +188,15 @@ public class FacebookTracker {
         Log.i(TAG, "TRACK CHECKOUT FINISHED: " + total);
         Bundle params = new Bundle();
 
-        params.putDouble(EVENT_PARAM_VALUE_TO_SUM, total);
-        params.putStringArrayList(AppEventsConstants.EVENT_PARAM_CONTENT_ID, skus);
-        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_TYPE, PRODUCT_CONTENT_TYPE);
-        params.putString(AppEventsConstants.EVENT_PARAM_CURRENCY, CurrencyFormatter.EURO_CODE);
-        params.putString(EVENT_PARAM_SHOP_COUNTRY, shopCountry);
-        params.putString(EVENT_PARAM_APP_VERSION, appVersion);
-        logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, total, params);
+//        params.putDouble(EVENT_PARAM_VALUE_TO_SUM, total);
+//        params.putStringArrayList(AppEventsConstants.EVENT_PARAM_CONTENT_ID, skus);
+//        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_TYPE, PRODUCT_CONTENT_TYPE);
+//        params.putString(AppEventsConstants.EVENT_PARAM_CURRENCY, CurrencyFormatter.EURO_CODE);
+//        params.putString(EVENT_PARAM_SHOP_COUNTRY, shopCountry);
+//        params.putString(EVENT_PARAM_APP_VERSION, appVersion);
+//        logEvent(AppEventsConstants.EVENT_NAME_PURCHASED, total, params);
+
 //        mFacebookLogger.logPurchase(BigDecimal.valueOf(total), Currency.getInstance(CurrencyFormatter.EURO_CODE), params);
     }
-
-
-    //    /**
-//     * Track added item to wishlist.
-//     * @param sku
-//     * @param price
-//     * @param shopCountry
-//     * @param appVersion
-//     * @author sergiopereira
-//     */
-//    public void trackAddedToWishlist(String sku, double price, String shopCountry, String appVersion) {
-//        Log.i(TAG, "TRACK ADD TO WISHLIST: " + sku + " " + price);
-//        trackItem(AppEventsConstants.EVENT_NAME_ADDED_TO_WISHLIST, sku, price, shopCountry, appVersion);
-//    }
-
-
-//    /**
-//     * Track checkout started.
-//     * @param userId
-//     * @param total
-//     * @param numItems
-//     * @author sergiopereira
-//     */
-//    public void trackCheckoutStarted(String userId, double total, int numItems) {
-//        Log.i(TAG, "TRACK CHECKOUT STARTED: " + total + " " + numItems);
-//        Bundle params = new Bundle();
-//        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_ID, userId);
-//        params.putInt(AppEventsConstants.EVENT_PARAM_NUM_ITEMS, numItems);
-//        params.putString(AppEventsConstants.EVENT_PARAM_CURRENCY, CurrencyFormatter.EURO_CODE);
-//        logEvent(AppEventsConstants.EVENT_NAME_INITIATED_CHECKOUT, total, params);
-//    }
-
-
-
-//    /**
-//     * Track rating.
-//     * @param sku
-//     * @param category
-//     * @param rateValue
-//     * @param maxRate
-//     * @author sergiopereira
-//     */
-//    public void trackRated(String sku, String category, double rateValue, int maxRate) {
-//        Log.i(TAG, "TRACK RATE: " + sku + " " + rateValue);
-//        Bundle params = new Bundle();
-//        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_ID, sku);
-//        params.putString(AppEventsConstants.EVENT_PARAM_CONTENT_TYPE, category);
-//        params.putInt(AppEventsConstants.EVENT_PARAM_MAX_RATING_VALUE, maxRate);
-//        logEvent(AppEventsConstants.EVENT_NAME_RATED, rateValue, params);
-//    }
-//
-//    /**
-//     * Track search.
-//     * @param query
-//     * @author sergiopereira
-//     */
-//    public void trackSearched(String query) {
-//        Log.i(TAG, "TRACK SEARCH: " + query);
-//        Bundle params = new Bundle();
-//        params.putString(AppEventsConstants.EVENT_PARAM_SEARCH_STRING, query);
-//        logEvent(AppEventsConstants.EVENT_NAME_SEARCHED, params);
-//    }
 
 }
