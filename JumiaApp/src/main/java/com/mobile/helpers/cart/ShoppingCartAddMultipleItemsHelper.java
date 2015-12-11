6e52e7b2693d22446b6687b165689ce0900db2d6
alexandrapires <alexandra.pires@africainternetgroup.com>
2015-12-11 10:34:03 +0000

    % updated cart/addmultimple request

--- JumiaApp/src/main/java/com/mobile/helpers/cart/ShoppingCartAddMultipleItemsHelper.java
+++ JumiaApp/src/main/java/com/mobile/helpers/cart/ShoppingCartAddMultipleItemsHelper.java
@@ -9,20 +9,21 @@ import com.mobile.newFramework.pojo.BaseResponse;
 import com.mobile.newFramework.requests.BaseRequest;
 import com.mobile.newFramework.requests.RequestBundle;
 import com.mobile.newFramework.rest.interfaces.AigApiInterface;
-import com.mobile.newFramework.utils.CollectionUtils;
+import com.mobile.newFramework.utils.Constants;
 import com.mobile.newFramework.utils.EventTask;
 import com.mobile.newFramework.utils.EventType;
 import com.mobile.newFramework.utils.output.Print;
 import com.mobile.utils.TrackerDelegator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
 
     private static String TAG = ShoppingCartAddMultipleItemsHelper.class.getSimpleName();
 
+    public static String ADD_ITEMS = "add_items";
+
+    /*
     public static String PRODUCT_LIST_TAG = "productList";
 
     public static String getProductListSimpleSkuTag(int counter) {
@@ -33,9 +34,9 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
         return PRODUCT_LIST_TAG + "[" + counter + "]" + "[" + ShoppingCartAddItemHelper.PRODUCT_TAG + "]";
     }
 
-    public static String ADD_ITEMS = "add_items";
+    public static String ADD_ITEMS = "add_items";*/
 
-    private HashMap<String, String> productBySku;
+//    private HashMap<String, String> productBySku;
 
 
     @Override
@@ -48,24 +49,26 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
         return EventTask.ACTION_TASK;
     }
 
+    /*
     @Override
     protected Map<String, String> getRequestData(Bundle args) {
         productBySku = (HashMap<String, String>) args.getSerializable(ADD_ITEMS);
         return createValues(productBySku);
-    }
+    }*/
 
     @Override
     public void onRequest(RequestBundle requestBundle) {
         new BaseRequest(requestBundle, this).execute(AigApiInterface.addMultipleItemsShoppingCart);
     }
 
+    /*
     @Override
     public void postError(BaseResponse baseResponse) {
         super.postError(baseResponse);
         AddMultipleStruct addMultipleStruct = new AddMultipleStruct();
         handleError(baseResponse, addMultipleStruct);
         baseResponse.getMetadata().setData(addMultipleStruct);
-    }
+    }*/
 
     @Override
     public void postSuccess(BaseResponse baseResponse) {
@@ -81,10 +84,24 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
 //        bundle.putParcelable(Constants.BUNDLE_RESPONSE_KEY, cart);
         AddMultipleStruct addMultipleStruct = new AddMultipleStruct();
         addMultipleStruct.setPurchaseEntity(cart);
-        handleSuccess(baseResponse, addMultipleStruct);
+ //       handleSuccess(baseResponse, addMultipleStruct);
         baseResponse.getMetadata().setData(addMultipleStruct);
     }
 
+
+    /**
+     * Method used to create a request bundle.
+     */
+    public static Bundle createBundle(ArrayList <String> requestDataList) {
+        // Request data
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList(Constants.BUNDLE_ARRAY_KEY, requestDataList);
+        return bundle;
+    }
+
+
+
+    /*
     private Map<String, String> createValues(HashMap<String, String> values) {
         int counter = 0;
         Map<String, String> data = new HashMap<>();
@@ -94,9 +111,9 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
             counter++;
         }
         return data;
-    }
+    }*/
 
-
+/*
     //@Override
     protected void handleSuccess(BaseResponse baseResponse, AddMultipleStruct struct) {
         Map successMessages = baseResponse.getSuccessMessages();
@@ -104,22 +121,23 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
             struct.setSuccessMessages(checkAddedProducts(successMessages));
         }
         handleError(baseResponse, struct);
-    }
+    }*/
 
     //@Override
-    protected void handleError(BaseResponse baseResponse, AddMultipleStruct struct) {
+
+ /*   protected void handleError(BaseResponse baseResponse, AddMultipleStruct struct) {
         Map errorMessages = baseResponse.getErrorMessages();
         if (CollectionUtils.isNotEmpty(errorMessages)) {
             struct.setErrorMessages(checkNotAddedProducts(errorMessages));
         }
-    }
+    }*/
 
     /**
      * Check added products from result object
      *
      * @return Array of products sku's
      */
-    protected ArrayList<String> checkAddedProducts(Map<String, String> successMessages) {
+ /*   protected ArrayList<String> checkAddedProducts(Map<String, String> successMessages) {
         ArrayList<String> added = new ArrayList<>();
         for (Map.Entry<String, String> entry : successMessages.entrySet()) {
             String value = productBySku.get(entry.getKey());
@@ -128,14 +146,14 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
             }
         }
         return added;
-    }
+    }*/
 
     /**
      * Check products that were not added from result object
      *
      * @return Array of products sku's
      */
-    protected ArrayList<String> checkNotAddedProducts(Map<String, String> errorMessages) {
+/*    protected ArrayList<String> checkNotAddedProducts(Map<String, String> errorMessages) {
         ArrayList<String> notAdded = new ArrayList<>();
         for (Map.Entry<String, String> entry : errorMessages.entrySet()) {
             String value = productBySku.get(entry.getKey());
@@ -144,7 +162,9 @@ public class ShoppingCartAddMultipleItemsHelper extends SuperBaseHelper {
             }
         }
         return notAdded;
-    }
+    }*/
+
+
 
     public class AddMultipleStruct {
         private PurchaseEntity purchaseEntity;
