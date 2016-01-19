0c58ab568c6941436ffe9cc2c0fa5cbaf3311e0d
Sergio Pereira <sergio.pereira@africainternetgroup.com>
2016-01-19 16:27:54 +0000

    % Updated  RTL layouts (Bamilo and Daraz).

--- JumiaApp/src/main/java/com/mobile/utils/CheckoutStepManager.java
+++ JumiaApp/src/main/java/com/mobile/utils/CheckoutStepManager.java
@@ -89,7 +89,6 @@ public class CheckoutStepManager {
     /**
      * Method used to set the total bar for MyAccount.
      */
-    @Deprecated
     public static void setTotalBarForMyAccount(@NonNull View view) {
         // Hide total view
         view.findViewById(R.id.checkout_total_label).setVisibility(View.GONE);
