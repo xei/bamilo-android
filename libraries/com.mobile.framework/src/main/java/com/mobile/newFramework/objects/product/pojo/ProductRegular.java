875961ac9abc8ad87b9c0c22fe1accea54eb6689
Paulo Carvalho <paulo.carvalho@africainternetgroup.com>
2016-01-26 14:54:49 +0000

    improved documentation;

--- libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/objects/product/pojo/ProductRegular.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/objects/product/pojo/ProductRegular.java
@@ -52,6 +52,7 @@ public class ProductRegular extends ProductBase {
     protected final boolean initializeProductRegular(JSONObject jsonObject) throws JSONException {
         // Mandatory
         mName = jsonObject.optString(RestConstants.NAME);
+        // TODO
         // This fallback validation was added for the case where the response from the RR request
         // is the fallback top sellers, where the product name comes in the "title" field.
         // it should be analysed if should be fixed on next release.
