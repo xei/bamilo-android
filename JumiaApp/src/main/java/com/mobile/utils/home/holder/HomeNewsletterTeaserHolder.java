b335bfc0f9ebd840709d8b9332b464ffa65efdac
manuelsilvaaig <manuel.silva@africainternetgroup.com>
2016-03-21 15:49:53 +0000

    - NAFAMZ-16581 Android :: Bug :: Newsletter sign-in form on Home screen.

--- JumiaApp/src/main/java/com/mobile/utils/home/holder/HomeNewsletterTeaserHolder.java
+++ JumiaApp/src/main/java/com/mobile/utils/home/holder/HomeNewsletterTeaserHolder.java
@@ -174,7 +174,6 @@ public class HomeNewsletterTeaserHolder extends BaseTeaserViewHolder {
         } else if(mGenderSpinner != null){
             return sInitialGender = mGenderSpinner.getSelectedItemPosition();
         }
-
         return 0;
     }
 }
