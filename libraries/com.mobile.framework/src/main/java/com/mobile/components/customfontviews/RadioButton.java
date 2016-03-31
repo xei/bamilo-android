e3fe026abb585bb6bd0ef8adce6d2f5c9398a538
manuelsilvaaig <manuel.silva@africainternetgroup.com>
2016-03-31 11:51:10 +0100

    - NAFAMZ-16440  Android: Update "Radio Button" Elements to match latest style guide and component updates

--- libraries/com.mobile.framework/src/main/java/com/mobile/components/customfontviews/RadioButton.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/components/customfontviews/RadioButton.java
@@ -2,14 +2,19 @@
 package com.mobile.components.customfontviews;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.support.annotation.ColorInt;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
 
 import com.mobile.components.customfontviews.HoloFontLoader.FontStyleProvider;
 
-public class RadioButton extends android.widget.RadioButton implements FontStyleProvider {
+public class RadioButton extends AppCompatRadioButton implements FontStyleProvider {
     private String mFontFamily;
 
     private int mFontStyle;
+    @ColorInt public static final int BLACK_800    = 0xFF808080;
+    @ColorInt public static final int BLUE1        = 0xFF416998;
 
     public RadioButton(Context context) {
         this(context, null);
@@ -22,6 +27,19 @@ public class RadioButton extends android.widget.RadioButton implements FontStyle
     public RadioButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         TextView.construct(this, context, attrs, defStyle);
+        ColorStateList colorStateList = new ColorStateList(
+                new int[][]{
+                        new int[]{-android.R.attr.state_checked},
+                        new int[]{android.R.attr.state_checked}
+                },
+                new int[]{
+
+                        BLACK_800
+                        , BLUE1
+                }
+        );
+        this.setSupportButtonTintList(colorStateList);
+
     }
 
     @Override
