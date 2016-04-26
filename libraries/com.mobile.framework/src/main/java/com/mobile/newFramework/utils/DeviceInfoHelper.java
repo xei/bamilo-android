fde60f266b7c8c9042ae4e2fd48cf0898c6148e4
manuelsilvaaig <manuel.silva@africainternetgroup.com>
2016-04-26 17:32:27 +0100

    - NAFAMZ-16783 Android:: Category Menu :: Display "external links" items set-up in the "App category mgr"

--- libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/utils/DeviceInfoHelper.java
+++ libraries/com.mobile.framework/src/main/java/com/mobile/newFramework/utils/DeviceInfoHelper.java
@@ -230,9 +230,11 @@ public class DeviceInfoHelper {
     public static boolean hasTelephony(Context context) {
         PackageManager pm = context.getPackageManager();
         TelephonyManager tm= (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY) && tm.getPhoneType()!=TelephonyManager.PHONE_TYPE_NONE){
+
+        if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY) && tm.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE && tm.getSimState() != TelephonyManager.SIM_STATE_ABSENT){
            return true;
         }
+
         return false;
     }
     
