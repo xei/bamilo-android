93f3ff4c715b43239429cc412597c5b3ad0d186e
ArashHassanpour <arash.hassanpour@bamilo.com>
2017-03-15 10:26:25 +0330

    Progress component implemented

--- JumiaApp/src/main/java/com/mobile/view/newfragments/OrderTrackingHeader.java
+++ JumiaApp/src/main/java/com/mobile/view/newfragments/OrderTrackingHeader.java
@@ -1,10 +1,20 @@
 package com.mobile.view.newfragments;
 
 import android.content.Context;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.mobile.components.customfontviews.TextView;
+import com.mobile.newFramework.objects.orders.OrderTrackerItem;
+import com.mobile.view.R;
+
 import java.util.ArrayList;
+
+import static com.mobile.view.R.id.product;
+import static com.mobile.view.newfragments.OrderStateType.PROGRESS_ITEM_ACTIVE;
 
 /**
  * Created by User on 3/12/2017.
@@ -12,9 +22,13 @@ import java.util.ArrayList;
 
 public class OrderTrackingHeader {
     public ArrayList<OrderStateStep> steps;
+    private Context context;
+    private ViewGroup parent;
 
-    public OrderTrackingHeader()
+    public OrderTrackingHeader(Context context, ViewGroup parent)
     {
+        this.context = context;
+        this.parent = parent;
         steps = new ArrayList<>();
     }
 
@@ -23,16 +37,122 @@ public class OrderTrackingHeader {
         steps.add(placement-1, step);
     }
 
-    public View createHeader(Context context, RelativeLayout holder)
+    public void createHeader(Context context)
     {
-        for (OrderStateStep step: steps) {
-            View stepView = step.getStep(context, holder);
-            holder.addView(stepView);
+        OrderStatus orderStatus = getOrderStatusFromOrderProducts(null);
+
+        OrderStateStep step = new OrderStateStep("ثبت سفارش", 1, R.drawable.order_list_grey, R.drawable.order_list_green,
+                R.drawable.order_list_white, 0);
+        addStep(step, 1);
+
+        step = new OrderStateStep("در حال تامین", 2, R.drawable.order_package_grey, R.drawable.order_package_green,
+                R.drawable.order_package_white, 0);
+        addStep(step, 2);
+
+        step = new OrderStateStep("ارسال شد", 3, R.drawable.order_truck_grey, R.drawable.order_truck_green,
+                R.drawable.order_truck_white, R.drawable.order_truck_red);
+        addStep(step, 3);
+
+        steps.get(0).setType(OrderStateType.PROGRESS_ITEM_PENDING);
+        steps.get(1).setType(OrderStateType.PROGRESS_ITEM_PENDING);
+        steps.get(2).setType(OrderStateType.PROGRESS_ITEM_PENDING);
+
+        switch (orderStatus) {
+            case ORDER_STATUS_NEW_ORDER: {
+                steps.get(0).setType(OrderStateType.PROGRESS_ITEM_ACTIVE);
+            }
+            break;
+
+            case ORDER_STATUS_REGISTERED: {
+                steps.get(0).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(1).setType(OrderStateType.PROGRESS_ITEM_ACTIVE);
+            }
+            break;
+
+            case ORDER_STATUS_IN_PROGRESS: {
+                steps.get(0).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(1).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(2).setType(OrderStateType.PROGRESS_ITEM_ACTIVE);
+            }
+            break;
+
+            case ORDER_STATUS_DELIVERED: {
+                steps.get(0).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(1).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(2).setType(OrderStateType.PROGRESS_ITEM_DONE);
+                steps.get(2).setLabel("تحویل شد");
+            }
+            break;
+
+            case ORDER_STATUS_CANCELLED: {
+                steps.get(0).setType(OrderStateType.PROGRESS_ITEM_PENDING);
+                steps.get(1).setType(OrderStateType.PROGRESS_ITEM_PENDING);
+                steps.get(2).setType(OrderStateType.PROGRESS_ITEM_ERROR);
+                steps.get(2).setLabel("لغو شد");
+            }
+            break;
+
+            default:
+                break;
         }
 
 
+        View step1 = parent.findViewById(R.id.order_step1);
+        steps.get(0).getStep(context, step1);
+
+        View step2 = parent.findViewById(R.id.order_step2);
+        steps.get(1).getStep(context, step2);
+
+        View step3 = parent.findViewById(R.id.order_step3);
+        steps.get(2).getStep(context, step3);
+
+        TextView step1Title = (TextView) parent.findViewById(R.id.order_step1_title);
+        step1Title.setText(steps.get(0).getLabel());
+        step1Title.setTextColor(context.getResources().getColor(steps.get(0).getLabelColor()));
+        step1Title.setTextSize(TypedValue.COMPLEX_UNIT_SP, steps.get(0).getLabelSize());
 
 
-        return holder;
+        TextView step2Title = (TextView) parent.findViewById(R.id.order_step2_title);
+        step2Title.setText(steps.get(1).getLabel());
+        step2Title.setTextColor(context.getResources().getColor(steps.get(1).getLabelColor()));
+        step2Title.setTextSize(TypedValue.COMPLEX_UNIT_SP, steps.get(1).getLabelSize());
+
+        TextView step3Title = (TextView) parent.findViewById(R.id.order_step3_title);
+        step3Title.setText(steps.get(2).getLabel());
+        step3Title.setTextColor(context.getResources().getColor(steps.get(2).getLabelColor()));
+        step3Title.setTextSize(TypedValue.COMPLEX_UNIT_SP, steps.get(2).getLabelSize());
+
+
+
+
+    }
+
+    private OrderStatus getOrderItemStatus(String status)
+    {
+        if(status.contains("سفارش جدید")) { //0
+            return OrderStatus.ORDER_STATUS_NEW_ORDER;
+            } else if(status.contains("در حال پردازش")) { //1
+                return OrderStatus.ORDER_STATUS_REGISTERED;
+            } else if(status.contains("ارسال شد")) { //2
+                return OrderStatus.ORDER_STATUS_IN_PROGRESS;
+            } else if(status.contains("تحویل داده شد")) { //3
+                return OrderStatus.ORDER_STATUS_DELIVERED;
+            } else if(status.contains("لغو شده")) { //4
+                return OrderStatus.ORDER_STATUS_CANCELLED;
+            }
+
+        return OrderStatus.ORDER_STATUS_NONE;
+    }
+
+    private OrderStatus getOrderStatusFromOrderProducts(ArrayList<OrderTrackerItem> products) {
+        int currentOrderStep = 99;
+        if (products == null)
+        {
+            return OrderStatus.ORDER_STATUS_NONE;
+        }
+        for(OrderTrackerItem product : products) {
+            currentOrderStep = Math.min(currentOrderStep, getOrderItemStatus(product.getStatus()).getValue());
+        }
+        return OrderStatus.fromInt(currentOrderStep);
     }
 }
