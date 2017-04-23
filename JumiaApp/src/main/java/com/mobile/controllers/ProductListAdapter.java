e3259fdb03fe11f8061051d6facdd3eb8ee0349f
Narbeh Mirzaei <narbeh.mirzaei@bamilo.com>
2017-04-23 17:57:44 +0430

    Implemented ImageFetcher and ImageManager. Catalog view is using it now

--- JumiaApp/src/main/java/com/mobile/controllers/ProductListAdapter.java
+++ JumiaApp/src/main/java/com/mobile/controllers/ProductListAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobile.newFramework.objects.product.pojo.ProductRegular;
+import com.mobile.utils.imageloader.ImageManager;
 import com.mobile.utils.imageloader.RocketImageLoader;
 import com.mobile.utils.product.UIProductUtils;
 import com.mobile.utils.ui.ProductListViewHolder;
@@ -50,7 +51,9 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListViewHold
         //Show/Hide Shop First
         UIProductUtils.setShopFirst(item,holder.shopFirst);
         // Set image
-        RocketImageLoader.instance.loadImage(item.getImageUrl(), holder.image, holder.progress, R.drawable.no_image_small);
+        //RocketImageLoader.instance.loadImage(item.getImageUrl(), holder.image, holder.progress, R.drawable.no_image_small);
+        //(imageUrl, false, imageView, progressView, placeHolderImageId, false, null, false);
+        ImageManager.getInstance().loadImage(this.mContext, item.getImageUrl(), false, holder.image, holder.progress, R.drawable.no_image_small, false, null, false);
         // Set is favorite image
         setFavourite(holder, item, position);
         // Set rating and reviews
---
+++ JumiaApp/src/main/java/com/mobile/utils/imageloader/ImageManager.java
@@ -0,0 +1,145 @@
+package com.mobile.utils.imageloader;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.mobile.view.R;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+/**
+ * Created by Narbeh M. on 4/23/17.
+ */
+
+public final class ImageManager {
+    private ImageLoader mImageLoader;
+
+    private static ImageManager instance = null;
+    protected ImageManager() {
+        // Exists only to defeat instantiation.
+    }
+    public static ImageManager getInstance() {
+        if(instance == null) {
+            instance = new ImageManager();
+        }
+        return instance;
+    }
+
+    public void loadImage(Context context, final String imageUrl, boolean isFilePathList, final NetworkImageView imageView, final View progressView, final int placeHolderImageId, final boolean hideImageView, final RocketImageLoader.RocketImageLoaderListener listener, boolean isDraftImage) {
+        if (!TextUtils.isEmpty(imageUrl) && imageView !=  null) {
+            if(isDraftImage) {
+                if (progressView != null) {
+                    progressView.setVisibility(View.GONE);
+                }
+            } else if (isFilePathList) {
+                imageView.setImageBitmap(BitmapFactory.decodeFile(imageUrl));
+
+                if (progressView != null) {
+                    progressView.setVisibility(View.GONE);
+                }
+            } else {
+                ImageLoader.ImageContainer imgContainer = (ImageLoader.ImageContainer) imageView.getTag();
+                if (null != imgContainer && !imgContainer.getRequestUrl().equals(imageUrl)) {
+                    imgContainer.cancelRequest();
+                    if (listener != null) {
+                        listener.onLoadedCancel();
+                    }
+                }
+
+                if (progressView != null) {
+                    progressView.setVisibility(View.VISIBLE);
+                }
+
+                // clear any previous image
+                if(placeHolderImageId != -1) {
+                    imageView.setImageResource(placeHolderImageId);
+                }
+
+                mImageLoader = ImageFetcher.getInstance(context).getImageLoader();
+                mImageLoader.get(imageUrl, this.getImageListener(context, imageUrl, imageView, placeHolderImageId, android.R.drawable.ic_dialog_alert, progressView, hideImageView, listener));
+                imageView.setImageUrl(imageUrl, mImageLoader);
+                imageView.setTag(imgContainer);
+            }
+        } else if (imageView != null) {
+            if (progressView != null) {
+                progressView.setVisibility(View.GONE);
+            }
+
+            // clear any previous image
+            if(placeHolderImageId != -1) {
+                imageView.setImageResource(placeHolderImageId);
+            }
+
+            if (listener != null) {
+                listener.onLoadedError();
+            }
+        }
+    }
+
+    private ImageLoader.ImageListener getImageListener(final Context context, final String imageUrl, final NetworkImageView imageView, final int defaultImageResId, final int errorImageResId, final View progressView, final boolean hideImageView, final RocketImageLoader.RocketImageLoaderListener listener) {
+        return new ImageLoader.ImageListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (errorImageResId != 0) {
+                    imageView.setImageResource(errorImageResId);
+                }
+
+                if (progressView != null) {
+                    progressView.setVisibility(View.GONE);
+                }
+
+                if (listener != null) {
+                    listener.onLoadedError();
+                }
+
+                if (hideImageView) {
+                    imageView.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                if (response.getBitmap() != null) {
+                    imageView.setImageBitmap(response.getBitmap());
+                } else if (defaultImageResId != 0) {
+                    imageView.setImageResource(defaultImageResId);
+                }
+
+                if (null != response.getBitmap() && response.getBitmap().getWidth() != -1) {
+                    if (response.getRequestUrl().equals(imageUrl)) {
+                        if (progressView != null) {
+                            progressView.setVisibility(View.GONE);
+                        }
+
+                        // Animation
+                        Boolean noAnimate = (Boolean) imageView.getTag(R.id.no_animate);
+                        if (noAnimate == null || noAnimate) {
+                            imageView.setImageBitmap(response.getBitmap());
+                        } else {
+                            Animation animation = AnimationUtils.loadAnimation(context, R.anim.abc_fade_in);
+                            imageView.setImageBitmap(response.getBitmap());
+                            imageView.startAnimation(animation);
+                        }
+
+                        if (listener != null) {
+                            listener.onLoadedSuccess(imageUrl, response.getBitmap());
+                        }
+
+                        if (hideImageView) {
+                            imageView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                } else {
+                    //imageView.setImageBitmap(null);
+                    if(defaultImageResId != -1)
+                        imageView.setImageResource(defaultImageResId);
+                }
+            }
+        };
+    }
+}
