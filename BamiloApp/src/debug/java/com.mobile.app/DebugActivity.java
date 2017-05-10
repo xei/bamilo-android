e319be515d0a53bfde26663216250377451b65a7
Narbeh Mirzaei <narbeh@Narbehs-Mac-mini.local>
2017-05-10 13:14:13 +0430

    DROID-116: - Removed Debug Drawer from the project

--- BamiloApp/src/debug/java/com.mobile.app/DebugActivity.java
+++ BamiloApp/src/debug/java/com.mobile.app/DebugActivity.java
@@ -2,24 +2,17 @@ package com.mobile.app;
 
 import android.os.Bundle;
 
-import com.mobile.app.drawer.AbcDebugDrawerView;
-import com.mobile.app.drawer.DebugMobileApiModel;
 import com.mobile.utils.MyMenuItem;
 import com.mobile.utils.NavigationAction;
 import com.mobile.view.BaseActivity;
-import com.pushwoosh.PushManager;
 
 import java.util.Set;
-
-import io.palaima.debugdrawer.DebugDrawer;
 
 /**
  * DebugActivity
  * @author sergiopereira
  */
-public abstract class DebugActivity extends BaseActivity implements DebugMobileApiModel.ICustomMobileApi {
-
-    private DebugDrawer mDebugDrawer;
+public abstract class DebugActivity extends BaseActivity {
 
     /**
      * Constructor used to initialize the navigation list component and the autocomplete handler
@@ -32,36 +25,30 @@ public abstract class DebugActivity extends BaseActivity implements DebugMobileA
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Create debug drawer
-        mDebugDrawer = AbcDebugDrawerView.onCreate(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Start debug drawer
-        AbcDebugDrawerView.onStart(mDebugDrawer);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         // Resume debug drawer
-        AbcDebugDrawerView.onResume(mDebugDrawer);
-
     }
 
     @Override
     public void onPause() {
         super.onPause();
         // Pause debug drawer
-        AbcDebugDrawerView.onPause(mDebugDrawer);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         // Stop debug drawer
-        AbcDebugDrawerView.onStop(mDebugDrawer);
     }
 
     @Override
