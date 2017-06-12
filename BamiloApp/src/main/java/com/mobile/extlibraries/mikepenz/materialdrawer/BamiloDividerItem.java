d815c0b57c018fc13c99882524d4aca690694700
ArashHassanpour <arash.hassanpour@bamilo.com>
2017-06-12 12:56:41 +0430

    DROID-139: - libraries change to extlibraries

---
+++ BamiloApp/src/main/java/com/mobile/extlibraries/mikepenz/materialdrawer/BamiloDividerItem.java
@@ -0,0 +1,43 @@
+package com.mobile.libraries.mikepenz.materialdrawer;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.model.AbstractDrawerItem;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mobile.view.R;
+
+/**
+ * Created by Arash Hassanpour on 6/12/2017.
+ */
+
+public class BamiloDividerItem extends AbstractDrawerItem<BamiloDividerItem, BamiloDividerItem.ViewHolder> {
+
+    @Override
+    public int getType() {
+        return R.id.material_drawer_item_divider;
+    }
+
+    @Override
+    @LayoutRes
+    public int getLayoutRes() {
+        return R.layout.drawer_item_divider;
+    }
+
+    @Override
+    public BamiloDividerItem.ViewHolder getViewHolder(View v) {
+        return new BamiloDividerItem.ViewHolder(v);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private View view;
+        private View divider;
+
+        private ViewHolder(View view) {
+            super(view);
+            this.view = view;
+            this.divider = view.findViewById(R.id.material_drawer_divider);
+        }
+    }
+}
