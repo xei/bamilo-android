2287fe1a17488631ea22ff163997fdbf9b94654a
Mohsen Bahman-Poor <mohsen.bahmanpour@bamilo.com>
2017-08-12 17:57:17 +0430

    DROID-165:
    - Delivery time rest api implemented
    - Data manager implemented to cache response into static var till app is running

---
+++ BamiloFramework/src/main/java/com/mobile/service/utils/ApiConstants.java
@@ -0,0 +1,6 @@
+package com.mobile.service.utils;
+
+public final class ApiConstants {
+    public static final String GET_REGIONS_API_PATH = "form_field_call::customer/getaddressregions/";
+    public static final String GET_CITIES_API_PATH = "form_field_call::customer/getaddresscities/region/%s/";
+}
