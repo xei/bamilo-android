8b6a7c96dccdf12a5120a1eaa19fd629b8c115a4
Mohsen Bahman-Poor <mohsen.bahmanpour@bamilo.com>
2017-08-27 21:48:45 +0430

    DROID-193:
    - campaign page scroll issue fixed

--- BamiloApp/src/main/java/com/mobile/utils/ui/UITabLayoutUtils.java
+++ BamiloApp/src/main/java/com/mobile/utils/ui/UITabLayoutUtils.java
@@ -100,7 +100,7 @@ public class UITabLayoutUtils {
                 || action == NavigationAction.MY_ACCOUNT_MY_ADDRESSES || action == NavigationAction.MY_ACCOUNT
                 || action == NavigationAction.MY_ACCOUNT_EMAIL_NOTIFICATION || action == NavigationAction.MY_ACCOUNT_USER_DATA
                 || action == NavigationAction.BASKET || action == NavigationAction.ABOUT
-                || action == NavigationAction.MY_ORDERS;
+                || action == NavigationAction.MY_ORDERS || action == NavigationAction.UNKNOWN;
     }
 
     public static int getTabPosition(@NavigationAction.Type int action) {
