f069005f28742e8178cda14191c748fe934b038b
Mohsen Bahman-Poor <mohsen.bahmanpour@bamilo.com>
2017-10-10 13:41:17 +0330

    DROID-222:
    - my bamilo categories drop down added

--- BamiloApp/src/main/java/com/mobile/view/fragments/MyBamiloFragment.java
+++ BamiloApp/src/main/java/com/mobile/view/fragments/MyBamiloFragment.java
@@ -1,19 +1,23 @@
 package com.mobile.view.fragments;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.support.v4.widget.NestedScrollView;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.ListPopupWindow;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.emarsys.predict.RecommendedItem;
 import com.mobile.adapters.RecommendGridAdapter;
 import com.mobile.adapters.RecommendItemsDiffUtilCallback;
+import com.mobile.components.customfontviews.TextView;
 import com.mobile.constants.ConstantsIntentExtra;
 import com.mobile.controllers.fragments.FragmentController;
 import com.mobile.controllers.fragments.FragmentType;
@@ -23,7 +27,6 @@ import com.mobile.extlibraries.emarsys.predict.recommended.RecommendManager;
 import com.mobile.view.R;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,6 +41,7 @@ public class MyBamiloFragment extends BaseFragment implements RecommendListCompl
     private Map<String, List<Item>> recommendItemsMap;
     private String selectedCategory = null;
     private boolean useDiffUtil = true;
+    private int recommendListScrollPosition;
 
     public MyBamiloFragment() {
         super(true, R.layout.fragment_my_bamilo);
@@ -55,6 +59,7 @@ public class MyBamiloFragment extends BaseFragment implements RecommendListCompl
             @Override
             public void onRefresh() {
                 useDiffUtil = false;
+                recommendListScrollPosition = 0;
                 recommendItemsMap = new HashMap<>();
                 requestForRecommendLists();
             }
@@ -63,17 +68,110 @@ public class MyBamiloFragment extends BaseFragment implements RecommendListCompl
         getBaseActivity().enableSearchBar(true, rvRecommendedItemsList);
         srlRecommendItemsList.setProgressViewOffset(false, rvRecommendedItemsList.getPaddingTop(), srlRecommendItemsList.getProgressViewEndOffset());
         srlRecommendItemsList.setColorSchemeResources(R.color.appBar);
-        rvRecommendedItemsList.setLayoutManager(new GridLayoutManager(getContext(), RECOMMEND_LIST_COLUMN_COUNT));
         recommendListItems = new ArrayList<>();
         recommendGridAdapter = new RecommendGridAdapter(recommendListItems, this);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), RECOMMEND_LIST_COLUMN_COUNT);
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                // determine that how many spans does the item need
+                int span = 1;
+                switch (recommendGridAdapter.getItemViewType(position)) {
+                    case RecommendGridAdapter.TYPE_CATEGORIES_DROP_DOWN:
+                        span = RECOMMEND_LIST_COLUMN_COUNT;
+                        break;
+                    case RecommendGridAdapter.TYPE_PRODUCT:
+                        span = 1;
+                        break;
+                }
+                return span;
+            }
+        });
+        rvRecommendedItemsList.setLayoutManager(layoutManager);
         rvRecommendedItemsList.setAdapter(recommendGridAdapter);
+        recommendGridAdapter.setOnCategoryDropdownClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ListPopupWindow listPopupWindow = new ListPopupWindow(getContext());
+                List<String> items = new ArrayList<String>();
+                items.add(getString(R.string.all_categories));
+                items.addAll(recommendItemsMap.keySet());
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.popup_menu_item, items) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                        if (convertView == null) {
+                            convertView = LayoutInflater.from(getContext()).inflate(R.layout.popup_menu_item, parent, false);
+                        }
+                        TextView tv = (TextView) convertView.findViewById(android.R.id.text1);
+                        tv.setText(getItem(position));
+                        if ((selectedCategory == null && position == 0)
+                                || (getItem(position) != null && getItem(position).equals(selectedCategory))) {
+                            convertView.findViewById(R.id.imgCheck).setVisibility(View.VISIBLE);
+                        } else {
+                            convertView.findViewById(R.id.imgCheck).setVisibility(View.INVISIBLE);
+                        }
+                        return convertView;
+                    }
+                };
+
+                listPopupWindow.setAnchorView(v.findViewById(R.id.menuAnchor));
+                listPopupWindow.setAdapter(adapter);
+                listPopupWindow.setWidth(v.getWidth());
+                listPopupWindow.setModal(true);
+                listPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        listPopupWindow.dismiss();
+                        if (position == 0) {
+                            selectedCategory = null;
+                        } else {
+                            List<String> categoryList = new ArrayList<String>(recommendItemsMap.keySet());
+                            selectedCategory = categoryList.get(position - 1);
+                        }
+                        recommendGridAdapter
+                                .setCategoryDropdownTitle(selectedCategory == null ? getString(R.string.all_categories) : selectedCategory);
+                        updateUi();
+                    }
+                });
+                listPopupWindow.show();
+                /*PopupMenu popupMenu = new PopupMenu(getContext(), v, Gravity.CENTER_HORIZONTAL);
+                Menu menu = popupMenu.getMenu();
+                for (String key : recommendItemsMap.keySet()) {
+                    menu.add(key);
+                }
+                popupMenu.show();*/
+            }
+        });
         if (recommendItemsMap == null) {
+            recommendListScrollPosition = 0;
             useDiffUtil = false;
             recommendItemsMap = new HashMap<>();
             requestForRecommendLists();
         } else {
             updateUi();
         }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        recommendListScrollPosition = ((GridLayoutManager) rvRecommendedItemsList.getLayoutManager()).findFirstVisibleItemPosition();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        rvRecommendedItemsList.post(new Runnable() {
+            @Override
+            public void run() {
+                if (recommendListScrollPosition == 0) {
+                    ((GridLayoutManager) rvRecommendedItemsList.getLayoutManager()).scrollToPositionWithOffset(recommendListScrollPosition, rvRecommendedItemsList.getPaddingTop());
+                } else {
+                    ((GridLayoutManager) rvRecommendedItemsList.getLayoutManager()).scrollToPosition(recommendListScrollPosition);
+                }
+            }
+        });
     }
 
     private void requestForRecommendLists() {
@@ -83,6 +181,9 @@ public class MyBamiloFragment extends BaseFragment implements RecommendListCompl
 
     @Override
     public void onRecommendedRequestComplete(String category, List<RecommendedItem> data) {
+        String CATEGORY_DELIMITER = ">";
+        String temp[] = category.split(CATEGORY_DELIMITER);
+        category = temp[temp.length - 1];
         srlRecommendItemsList.setRefreshing(false);
         List<Item> itemDataList = new ArrayList<>();
         for (RecommendedItem r : data) {
@@ -107,6 +208,9 @@ public class MyBamiloFragment extends BaseFragment implements RecommendListCompl
             }
         } else {
             itemsToShow = recommendItemsMap.get(selectedCategory);
+        }
+        if (itemsToShow == null) {
+            return;
         }
         RecommendItemsDiffUtilCallback callback = new RecommendItemsDiffUtilCallback(itemsToShow, recommendListItems);
         if (useDiffUtil) {
