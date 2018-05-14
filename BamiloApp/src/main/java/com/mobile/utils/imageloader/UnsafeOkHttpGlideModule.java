d8ace8e73af13a3f5d6cec3a8dae62ce875dff89
farshidabz <farshid.abazari@bamilo.com>
2018-05-14 11:30:12 +0430

    Fixed unloading images on old android versions

---
+++ BamiloApp/src/main/java/com/mobile/utils/imageloader/UnsafeOkHttpGlideModule.java
@@ -0,0 +1,27 @@
+package com.mobile.utils.imageloader;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.GlideModule;
+
+import java.io.InputStream;
+
+/**
+ * Created by Farshid
+ * since 5/13/2018.
+ * contact farshidabazari@gmail.com
+ */
+public class UnsafeOkHttpGlideModule implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        glide.register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory());
+    }
+}
