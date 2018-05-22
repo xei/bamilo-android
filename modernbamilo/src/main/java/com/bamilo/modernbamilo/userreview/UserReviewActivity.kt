d86eda59b5d4ef820f0b719598f8b1fe255ffb01
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-22 12:14:17 +0430

    add UserReviewActivity.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/UserReviewActivity.kt
@@ -0,0 +1,16 @@
+package com.bamilo.modernbamilo.userreview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bamilo.modernbamilo.R
+
+class UserReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_review)
+
+
+
+    }
+}
