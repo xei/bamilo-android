c182081c4e6305575ac6ff7d8ee9c33b3ef34906
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-22 14:41:43 +0430

    add WebApi interface for User Review.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/BamiloWebApi.kt
@@ -0,0 +1,22 @@
+package com.bamilo.modernbamilo.userreview
+
+import com.bamilo.modernbamilo.userreview.pojo.getsurvey.getSurveyResponse
+import com.bamilo.modernbamilo.userreview.pojo.getsurveylist.getSurveyListResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface BamiloWebApi {
+
+    /**
+     * Get a list of surveys which is available for a specific user.
+     */
+    @GET("survey/user/{userId}")
+    fun getSurveysList(@Path("userId") userId: String) : Call<getSurveyListResponse>
+
+    /**
+     * Gets a single survey by its alias.
+     */
+    @GET("survey/alias/{surveyAlias}")
+    fun getSurvey(@Path("surveyAlias") alias: String): Call<getSurveyResponse>
+}
