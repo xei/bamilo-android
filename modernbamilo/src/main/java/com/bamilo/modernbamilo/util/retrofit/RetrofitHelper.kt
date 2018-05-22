e259d0e3fa813dc6bab2b5527ebcac111aa38cfe
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-22 12:25:23 +0430

    add RetrofitHelper class.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/retrofit/RetrofitHelper.kt
@@ -0,0 +1,68 @@
+package com.bamilo.modernbamilo.util.retrofit
+
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * This object contains the necessary configurations for Retrofit and make the developers free of
+ * facing with the complexities. They can just call the method {@link makeWebApi} and all the things
+ * will happen.
+ *
+ * Created by Hamidreza Hosseinkhani on May 22, 2018 at Bamilo.
+ */
+object RetrofitHelper {
+    private const val URL_BASE = "http://bamilo.com/mobapi/v2.9/"
+    private val defaultHeaders = hashMapOf(
+//            "app-version" to BuildConfig.VERSION_CODE.toString(),
+            "locale" to "fa-ir"
+    )
+
+    private var retrofitInstance: Retrofit? = null
+
+    /**
+     * This method is responsible for creation of the web APIs via Retrofit.
+     */
+    fun<T> makeWebApi(service: Class<T>): T = getRetrofitInstance().create(service)
+
+    /**
+     * This method provides a single instance of Retrofit based on the base url, provided
+     * OkHttpClient and ConverterFactory
+     */
+    private fun getRetrofitInstance(): Retrofit {
+        if (retrofitInstance == null) {
+            retrofitInstance = Retrofit.Builder()
+                    .baseUrl(URL_BASE)
+                    .client(buildOkHttpClient())
+                    .addConverterFactory(buildConverterFactory())
+                    .build()
+        }
+
+        return retrofitInstance!!
+    }
+
+    /**
+     * This method builds an OkHttpClient instance with the default headers provided by
+     * the method "buildDefaultHeaders"
+     */
+    private fun buildOkHttpClient(): OkHttpClient {
+        return OkHttpClient().newBuilder()
+                .addInterceptor { chain ->
+                    val modifiedRequest = chain.request().newBuilder()
+                            .headers(Headers.of(defaultHeaders))
+                            .build()
+
+                    chain.proceed(modifiedRequest)
+                }
+                .build()
+    }
+
+    /**
+     * This method builds a converter factory (Gson, Protobuf, ...) to convert objects
+     * to/from a serialization.
+     */
+    private fun buildConverterFactory() : Converter.Factory = GsonConverterFactory.create()
+
+}
