c983fa9781cc292e26fefc31c3815f59db11a8d2
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-22 12:54:43 +0430

    add SharedPreferencesHelper.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/storage/SharedPreferencesHelper.kt
@@ -0,0 +1,20 @@
+package com.bamilo.modernbamilo.util.storage
+
+import android.content.Context
+
+
+/**
+ * This helper file is responsible for handling SharedPreferences operations entire the application.
+ *
+ * Created by Hamidreza Hosseinkhani on May 22, 2018 at Bamilo.
+ */
+private const val NAME_SHARED_PREFERENCES_SESSION = "persistent_cookies"
+
+private const val KEY_PREFERENCE_COOKIE = "cookie..bamilo.com"
+
+
+private fun getSessionSharedPreferences(context: Context)
+        = context.getSharedPreferences(NAME_SHARED_PREFERENCES_SESSION, Context.MODE_PRIVATE)
+
+fun getCookie(context: Context)
+        = getSessionSharedPreferences(context).getString(KEY_PREFERENCE_COOKIE, null)
