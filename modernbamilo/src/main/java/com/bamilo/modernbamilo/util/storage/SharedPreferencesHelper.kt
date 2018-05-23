0664628c5a7b11c01ac37b948912a918ed95b2aa
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-23 15:10:18 +0430

    check null reference in cookie shared preferences.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/util/storage/SharedPreferencesHelper.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/storage/SharedPreferencesHelper.kt
@@ -16,5 +16,4 @@ private const val KEY_PREFERENCE_COOKIE = "cookie..bamilo.com"
 private fun getSessionSharedPreferences(context: Context)
         = context.getSharedPreferences(NAME_SHARED_PREFERENCES_SESSION, Context.MODE_PRIVATE)
 
-fun getCookie(context: Context)
-        = getSessionSharedPreferences(context).getString(KEY_PREFERENCE_COOKIE, null)
+fun getCookie(context: Context): String? = getSessionSharedPreferences(context).getString(KEY_PREFERENCE_COOKIE, null)
