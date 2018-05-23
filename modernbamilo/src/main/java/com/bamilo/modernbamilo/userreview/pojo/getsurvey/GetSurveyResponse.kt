3c2b6bc149270f87ebbfdcc725a35d97fee36fb0
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-23 17:47:08 +0430

    rename getSurveyResponse to GetSurveyResponse

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurvey/GetSurveyResponse.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurvey/GetSurveyResponse.kt
@@ -2,6 +2,6 @@ package com.bamilo.modernbamilo.userreview.pojo.getsurvey
 
 import com.google.gson.annotations.SerializedName
 
-data class getSurveyResponse(
+data class GetSurveyResponse(
     @SerializedName("data") val data: Data
 )
