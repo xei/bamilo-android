490cc924cdd30be3770a4e85e45ab30a1ff3fa29
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-27 14:46:39 +0430

    rename getSurveyListResponse to GetSurveyListResponse.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/UserReviewWebApi.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/UserReviewWebApi.kt
@@ -1,7 +1,7 @@
 package com.bamilo.modernbamilo.userreview
 
-import com.bamilo.modernbamilo.userreview.pojo.getsurvey.getSurveyResponse
-import com.bamilo.modernbamilo.userreview.pojo.getsurveylist.getSurveyListResponse
+import com.bamilo.modernbamilo.userreview.pojo.getsurvey.GetSurveyResponse
+import com.bamilo.modernbamilo.userreview.pojo.getsurveylist.GetSurveyListResponse
 import com.bamilo.modernbamilo.util.retrofit.pojo.ResponseWrapper
 import retrofit2.Call
 import retrofit2.http.GET
@@ -13,12 +13,12 @@ interface UserReviewWebApi {
      * Get a list of surveys which is available for a specific user.
      */
     @GET("survey/user/{userId}")
-    fun getSurveysList(@Path("userId") userId: String) : Call<ResponseWrapper<getSurveyListResponse>>
+    fun getSurveysList(@Path("userId") userId: String) : Call<ResponseWrapper<GetSurveyListResponse>>
 
     /**
      * Gets a single survey by its alias.
      */
     @GET("survey/alias/journey_survey")
-    fun getSurvey(): Call<ResponseWrapper<getSurveyResponse>>
+    fun getSurvey(): Call<ResponseWrapper<GetSurveyResponse>>
 
 }
--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurveylist/GetSurveyListResponse.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurveylist/GetSurveyListResponse.kt
@@ -2,6 +2,6 @@ package com.bamilo.modernbamilo.userreview.pojo.getsurveylist
 
 import com.google.gson.annotations.SerializedName
 
-data class getSurveyListResponse(
+data class GetSurveyListResponse(
     @SerializedName("data") val data: Data
 )
