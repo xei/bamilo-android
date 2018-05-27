1ec0445a0a49e0bab16e7c5902b2a635196dbe00
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-27 13:04:10 +0430

    add transformer to ReviewPager.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/stepperview/ReviewPager.kt
@@ -0,0 +1,30 @@
+package com.bamilo.modernbamilo.userreview.stepperview
+
+import android.content.Context
+import android.util.AttributeSet
+import com.bamilo.modernbamilo.customview.RtlViewPager
+
+
+class ReviewPager : RtlViewPager {
+    constructor(context: Context) : super(context) {
+        setTransformationAnimation()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        setTransformationAnimation()
+    }
+
+    /**
+     * This method sets the transformer animation while swiping the pages.
+     * You can check out some animations in the following page:
+     * https://github.com/geftimov/android-viewpager-transformers
+     */
+    private fun setTransformationAnimation() {
+
+        setPageTransformer(false) { view, position ->
+            view.rotationY = position * -30
+        }
+
+    }
+
+}
