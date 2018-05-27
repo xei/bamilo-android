4eefa7cecd0bc828c4e8a80838e33557ecf5011f
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-27 15:12:30 +0430

    modify Message model in the responseWrapper.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/util/retrofit/pojo/Messages.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/retrofit/pojo/Messages.kt
@@ -2,7 +2,7 @@ package com.bamilo.modernbamilo.util.retrofit.pojo
 
 import com.google.gson.annotations.SerializedName
 data class Messages(
-        @SerializedName("successes") val successes: List<MessageItem>,
-        @SerializedName("messageItem") val messageItem: List<MessageItem>,
-        @SerializedName("validate") val validate: List<MessageItem>
+        @SerializedName("successes") val successes: MessageItem,
+        @SerializedName("messageItem") val messageItem: MessageItem,
+        @SerializedName("validate") val validate: MessageItem
 )
