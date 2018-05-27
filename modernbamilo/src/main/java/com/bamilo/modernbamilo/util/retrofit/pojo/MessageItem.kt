f597a37007b5d1c4dddb4eae10a3f15d5fc7b436
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-05-27 15:06:52 +0430

    rename Error class in the response wrapper model to MessageItem.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/util/retrofit/pojo/MessageItem.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/retrofit/pojo/MessageItem.kt
@@ -1,7 +1,7 @@
 package com.bamilo.modernbamilo.util.retrofit.pojo
 
 import com.google.gson.annotations.SerializedName
-data class Error(
+data class MessageItem(
     @SerializedName("reason") val reason: String,
     @SerializedName("message") val message: String,
     @SerializedName("code") val code: Int
