6d3d499e472e77c4f49b855f7da80bd5a007e585
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-02 19:25:19 +0430

    add HIDDEN type to ReviewPageType enum.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/ReviewPageType.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/ReviewPageType.kt
@@ -3,5 +3,6 @@ package com.bamilo.modernbamilo.userreview
 enum class ReviewPageType(str: String) {
     RADIO("RADIO"),
     CHECKBOX("CHECKBOX"),
-    ESSAY("ESSAY")
+    ESSAY("ESSAY"),
+    HIDDEN("HIDDEN")
 }
