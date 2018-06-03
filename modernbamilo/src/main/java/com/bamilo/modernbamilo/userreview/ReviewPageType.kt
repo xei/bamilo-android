240d1ec0172e4137857af0221aa757e8ec6524e3
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-03 16:56:32 +0430

    complete review page types enum.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/ReviewPageType.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/ReviewPageType.kt
@@ -3,7 +3,10 @@ package com.bamilo.modernbamilo.userreview
 enum class ReviewPageType(str: String) {
     RADIO("RADIO"),
     CHECKBOX("CHECKBOX"),
+    TEXTBOX("TEXTBOX"),
     ESSAY("ESSAY"),
+    IMAGE_SELECT("IMAGE_SELECT"),
+    NPS("NPS"),
     HIDDEN("HIDDEN"),
     THANKS("THANKS")
 }
