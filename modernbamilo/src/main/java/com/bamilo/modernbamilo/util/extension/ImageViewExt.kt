2d7af240e0ec523327f4e772fd8aed88dfded774
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-09 12:18:19 +0430

    add an extension to ImageView to load images from network.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/extension/ImageViewExt.kt
@@ -0,0 +1,8 @@
+package com.bamilo.modernbamilo.util.extension
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.loadImageFromNetwork(url: String) {
+    Glide.with(this).load(url).into(this)
+}
