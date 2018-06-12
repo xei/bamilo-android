b7159ec1fbcfd7b2a6a2be2beb93ecefd1ffc722
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-12 13:32:18 +0430

    add ReviewPageTypeEssayFragment

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/fragment/ReviewPageTypeEssayFragment.kt
@@ -0,0 +1,104 @@
+package com.bamilo.modernbamilo.userreview.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bamilo.modernbamilo.R
+import com.bamilo.modernbamilo.userreview.pojo.getsurvey.Question
+import com.bamilo.modernbamilo.util.extension.loadImageFromNetwork
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.fragment_review_page_type_essay.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ReviewPageTypeRadioFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class ReviewPageTypeEssayFragment : ReviewPageBaseFragment() {
+
+    private lateinit var mPageTitle: TextView
+    private lateinit var mProductImageImageView: ImageView
+    private lateinit var mViewModel: Question
+
+    private var mProductImageUrl: String? = null
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @return A new instance of fragment ReviewPageTypeRadioFragment.
+         */
+        @JvmStatic
+        fun newInstance(productImageUrl: String?) =
+                ReviewPageTypeEssayFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM_PRODUCT_IMAGE_URL, productImageUrl)
+                    }
+                }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            mProductImageUrl = it.getString(ARG_PARAM_PRODUCT_IMAGE_URL)
+        }
+
+        mViewModel = createMockData()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val rootView =  inflater.inflate(R.layout.fragment_review_page_type_essay, container, false)
+        mPageTitle = rootView.findViewById(R.id.fragmentReviewPageTypeEssay_xeiTextView_title)
+        mProductImageImageView = rootView.findViewById(R.id.fragmentReviewPageTypeEssay_imageView_productImage)
+
+
+        mPageTitle.text = mViewModel.title
+        if (mProductImageUrl != null) {
+            mProductImageImageView.loadImageFromNetwork(mProductImageUrl!!)
+            mProductImageImageView.visibility = View.VISIBLE
+        }
+
+        return rootView
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private fun createMockData(): Question {
+
+        val str = "{\n" +
+                "\"id\":7,\n" +
+                "\"title\":\"آیا نکته\u200Cای هست که مایل باشید با ما در میان بگذارید؟\",\n" +
+                "\"type\":\"ESSAY\",\n" +
+                "\"required\":false,\n" +
+                "\"hidden\":false,\n" +
+                "\"options\":[{\n" +
+                "}]\n" +
+                "}"
+        return Gson().fromJson(str, object : TypeToken<Question>() {}.type)
+    }
+}
