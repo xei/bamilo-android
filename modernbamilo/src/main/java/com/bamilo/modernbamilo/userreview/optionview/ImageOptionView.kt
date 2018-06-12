de226f3b0411d02e7e930643da7351cfbd0513df
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-12 13:33:42 +0430

    add ImageOptionView

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/optionview/ImageOptionView.kt
@@ -0,0 +1,13 @@
+package com.bamilo.modernbamilo.userreview.optionview
+
+import android.content.Context
+import android.widget.ImageView
+import com.bamilo.modernbamilo.R
+
+class ImageOptionView(context: Context) : ImageView(context) {
+
+    init {
+        setBackgroundResource(R.drawable.background_imageoption_survey)
+    }
+
+}
