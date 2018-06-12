c3e84e0fe4deafedd35ab5bdc2d002e67fafbc61
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-12 12:29:09 +0430

    check image url if it is empty.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/util/extension/ImageViewExt.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/util/extension/ImageViewExt.kt
@@ -4,5 +4,7 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImageFromNetwork(url: String) {
-    Glide.with(context).load(url).into(this)
+    if (!url.isEmpty()) {
+        Glide.with(context).load(url).into(this)
+    }
 }
