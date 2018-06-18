ef1ceed5eb341ed50d9f62dea125396f928ae7a4
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-18 19:14:50 +0430

    change model.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurveylist/Data.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/userreview/pojo/getsurveylist/Data.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 data class Data(
     @SerializedName("userId") val userId: Int, //0
     @SerializedName("orderNumber") val orderNumber: Int, //0
-    @SerializedName("surveys") val surveys: List<Survey>
+    @SerializedName("surveys") val surveys: List<com.bamilo.modernbamilo.userreview.pojo.getsurvey.Survey>
 )
