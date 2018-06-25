466026f1f8b62d5d60c289f2314c6d49ab9cfbda
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-06-25 15:40:18 +0430

    add PriceView

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/customview/PriceView.kt
@@ -0,0 +1,55 @@
+package com.bamilo.modernbamilo.customview
+
+import android.content.Context
+import android.graphics.Paint
+import android.util.AttributeSet
+import com.bamilo.modernbamilo.R
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+
+
+class PriceView: XeiTextView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet) {
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.PriceView)
+
+        try {
+            val needStrikeLine = typedArray.getBoolean(R.styleable.PriceView_strike, false)
+
+            if (needStrikeLine) {
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            }
+        } catch (e: Exception) {
+
+        } finally {
+            typedArray.recycle()
+        }
+
+    }
+
+    override fun setText(text: CharSequence?, type: BufferType?) =
+            super.setText(resources.getString(R.string.rial, format(text!!.trim())), type)
+
+
+    private fun format(price: CharSequence): String {
+        val symbols = DecimalFormatSymbols()
+        symbols.groupingSeparator = ','
+        val df = DecimalFormat()
+        df.decimalFormatSymbols = symbols
+        df.groupingSize = 3
+
+        return df.format(java.lang.Long.parseLong(price.toString()))
+    }
+
+}
