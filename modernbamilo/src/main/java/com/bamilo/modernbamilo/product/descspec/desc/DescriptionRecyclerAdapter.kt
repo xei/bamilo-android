f74997c87961cd45e5d8a4caaed498a1783fde50
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-01 17:51:54 +0430

    complete description fragment

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/product/descspec/desc/DescriptionRecyclerAdapter.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/descspec/desc/DescriptionRecyclerAdapter.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.bamilo.modernbamilo.R
 import com.bamilo.modernbamilo.customview.DateTimeView
 import com.bamilo.modernbamilo.product.descspec.desc.pojo.DescriptionRow
 import com.bamilo.modernbamilo.product.sellerslist.pojo.SellerViewModel
+import com.bamilo.modernbamilo.util.extension.loadImageFromNetwork
 import com.bamilo.modernbamilo.util.extension.persianizeDigitsInString
 import com.bamilo.modernbamilo.util.extension.persianizeNumberString
 
@@ -23,11 +25,42 @@ class DescriptionRecyclerAdapter(private val mDescriptionRows: ArrayList<Descrip
 
     override fun onBindViewHolder(holder: DescriptionViewHolder, position: Int) {
 
+        if (!mDescriptionRows[position].title.isNullOrEmpty()) {
+            holder.titleTextView.text = mDescriptionRows[position].title
+            holder.titleTextView.visibility = View.VISIBLE
+        } else {
+            holder.titleTextView.visibility = View.GONE
+        }
+
+        if (!mDescriptionRows[position].hint.isNullOrEmpty()) {
+            holder.hintTextView.text = mDescriptionRows[position].hint
+            holder.hintTextView.visibility = View.VISIBLE
+        } else {
+            holder.hintTextView.visibility = View.GONE
+        }
+
+        if (!mDescriptionRows[position].description.isNullOrEmpty()) {
+            holder.descriptionTextView.text = mDescriptionRows[position].description
+            holder.descriptionTextView.visibility = View.VISIBLE
+        } else {
+            holder.descriptionTextView.visibility = View.GONE
+        }
+
+        if (!mDescriptionRows[position].image.isNullOrEmpty()) {
+            holder.imageImageView.loadImageFromNetwork(mDescriptionRows[position].image!!)
+            holder.imageImageView.visibility = View.VISIBLE
+        } else {
+            holder.imageImageView.visibility = View.GONE
+        }
+
     }
 
     override fun getItemCount(): Int = mDescriptionRows.size
 
     inner class DescriptionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val titleTextView = itemView.findViewById(R.id.rowDescription_xeiTextView_title) as TextView
+        val hintTextView = itemView.findViewById(R.id.rowDescription_xeiTextView_hint) as TextView
+        val descriptionTextView = itemView.findViewById(R.id.rowDescription_xeiTextView_description) as TextView
+        val imageImageView = itemView.findViewById(R.id.rowDescription_imageView_image) as ImageView
     }
 }
