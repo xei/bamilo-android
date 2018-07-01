30b360461f80e0281f054b4dcfa532727b94c8a6
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-01 16:25:55 +0430

    add web api and pojos

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/descspec/DescSpecWebApi.kt
@@ -0,0 +1,23 @@
+package com.bamilo.modernbamilo.product.descspec
+
+import com.bamilo.modernbamilo.product.descspec.desc.pojo.DescriptionRow
+import com.bamilo.modernbamilo.util.retrofit.pojo.ResponseWrapper
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DescSpecWebApi {
+
+    /**
+     * Get a list of description rows.
+     */
+    @GET("product/description/{productId}?device=mobile_app")
+    fun getDescription(@Path("productId") userId: String) : Call<ResponseWrapper<ArrayList<DescriptionRow>>>
+
+    /**
+     * Get a list of description rows.
+     */
+    @GET("product/specification/{productId}?device=mobile_app")
+    fun getSpecification(@Path("productId") userId: String) : Call<ResponseWrapper<ArrayList<DescriptionRow>>>
+
+}
