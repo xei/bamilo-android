47db49b2b5e108496d5939b93f12eb2f17d154ed
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-03 17:30:29 +0430

    change the response type in web api

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/product/descspec/DescSpecWebApi.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/descspec/DescSpecWebApi.kt
@@ -1,6 +1,7 @@
 package com.bamilo.modernbamilo.product.descspec
 
 import com.bamilo.modernbamilo.product.descspec.desc.pojo.DescriptionRow
+import com.bamilo.modernbamilo.product.descspec.spec.pojo.SpecificationRow
 import com.bamilo.modernbamilo.util.retrofit.pojo.ResponseWrapper
 import retrofit2.Call
 import retrofit2.http.GET
@@ -18,6 +19,6 @@ interface DescSpecWebApi {
      * Get a list of description rows.
      */
     @GET("product/specification/{productId}?device=mobile_app")
-    fun getSpecification(@Path("productId") userId: String) : Call<ResponseWrapper<ArrayList<DescriptionRow>>>
+    fun getSpecification(@Path("productId") userId: String) : Call<ResponseWrapper<ArrayList<SpecificationRow>>>
 
 }
