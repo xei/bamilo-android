658751d8ad1f2470d89f0ff415921e63b43980ec
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-08 12:44:14 +0430

    add startActivity function to start SubmitRateActivity.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/product/rate/SubmitRateActivity.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/rate/SubmitRateActivity.kt
@@ -1,7 +1,18 @@
 package com.bamilo.modernbamilo.product.rate
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bamilo.modernbamilo.R
+
+private const val KEY_EXTRA_PRODUCT_ID = "KEY_EXTRA_PRODUCT_ID"
+
+fun startActivity(invokerContext: Context, productId: String) {
+    val intent = Intent(invokerContext, SubmitRateActivity::class.java)
+    intent.putExtra("", productId)
+    invokerContext.startActivity(intent)
+}
 
 class SubmitRateActivity : AppCompatActivity() {
 
