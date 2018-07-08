b6a0ff484848e8372f0c115f41641b3dac7e4e4b
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-08 12:57:21 +0430

    add debug log while starting the activity.

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/product/rate/SubmitRateActivity.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/rate/SubmitRateActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bamilo.modernbamilo.R
+import com.bamilo.modernbamilo.util.logging.Logger
+
+private const val TAG_DEBUG = "SubmitRateActivity"
 
 private const val KEY_EXTRA_PRODUCT_ID = "KEY_EXTRA_PRODUCT_ID"
 
@@ -12,6 +15,8 @@ fun startActivity(invokerContext: Context, productId: String) {
     val intent = Intent(invokerContext, SubmitRateActivity::class.java)
     intent.putExtra(KEY_EXTRA_PRODUCT_ID, productId)
     invokerContext.startActivity(intent)
+
+    Logger.log("SubmitRateActivity has started for product: $productId")
 }
 
 class SubmitRateActivity : AppCompatActivity() {
