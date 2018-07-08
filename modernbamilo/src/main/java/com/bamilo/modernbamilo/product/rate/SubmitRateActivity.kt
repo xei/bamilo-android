289856c90e0ff93c6298e7229712fb6b25c596e3
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-08 12:03:24 +0430

    add SubmitRateActivity.

---
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/rate/SubmitRateActivity.kt
@@ -0,0 +1,12 @@
+package com.bamilo.modernbamilo.product.rate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SubmitRateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_rate)
+    }
+}
