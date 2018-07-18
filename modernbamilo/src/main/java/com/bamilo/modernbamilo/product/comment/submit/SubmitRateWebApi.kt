43b0be4ac0ff777dcff53a899ed2416e91992629
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-07-18 13:45:03 +0430

    develop submit comment api

--- modernbamilo/src/main/java/com/bamilo/modernbamilo/product/comment/submit/SubmitRateWebApi.kt
+++ modernbamilo/src/main/java/com/bamilo/modernbamilo/product/comment/submit/SubmitRateWebApi.kt
@@ -1,20 +1,21 @@
 package com.bamilo.modernbamilo.product.comment.submit
 
+import com.bamilo.modernbamilo.userreview.pojo.SubmitSurveyResponse
 import com.bamilo.modernbamilo.util.retrofit.pojo.ResponseWrapper
 import retrofit2.Call
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface SubmitRateWebApi {
 
     /**
      * Submit a review for a product.
      */
-    @POST("product/rating/{productId}/{rating}/{title}/{content}?device=mobile_app")
-    fun submit(@Path("productId") userId: String,
-               @Path("rating") rating:Float,
-               @Path("title") title: String,
-               @Path("content") content: String
+    @POST("rating/addproductreview")
+    @FormUrlEncoded
+    fun submit(@Field(value="sku", encoded = false) productId: String,
+               @Field(value="rate", encoded = false) rating:Float,
+               @Field(value="title", encoded = false) title: String,
+               @Field(value="comment", encoded = false) content: String
     ) : Call<ResponseWrapper<Boolean>>
 
 }
