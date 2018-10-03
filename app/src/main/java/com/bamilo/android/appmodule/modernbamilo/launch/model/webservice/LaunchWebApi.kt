a9df39f413ca8fee6ca01073447bfecb62d01e95
Hamidreza Hosseinkhani <hamidreza.hosseinkhani@bamilo.com>
2018-10-03 15:59:42 +0330

    fix round btn in comments

--- app/src/main/java/com/bamilo/android/appmodule/modernbamilo/launch/model/webservice/LaunchWebApi.kt
+++ app/src/main/java/com/bamilo/android/appmodule/modernbamilo/launch/model/webservice/LaunchWebApi.kt
@@ -8,17 +8,19 @@ package com.bamilo.android.appmodule.modernbamilo.launch.model.webservice
 
 import com.bamilo.android.appmodule.modernbamilo.util.retrofit.pojo.ResponseWrapper
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface LaunchWebApi {
 
     /**
      * Get application startup configs.
      */
-    @POST("configurations")
-    fun getStartupConfigs(@Query("platform") platform: String,
-                          @Query("versionCode") versionCode: Int
+    @POST("configurations/")
+    @FormUrlEncoded
+    fun getStartupConfigs(@Field("platform") platform: String,
+                          @Field("versionCode") versionCode: Int
     ) : Call<ResponseWrapper<GetStartupConfigsResponse>>
 
 }
